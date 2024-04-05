05fdfb1f6b5fe46a73740ae5a85c6ff0a5be854b
molszewski34 <m.olszewski341@gmail.com>
2024-04-05 09:10:12 +0200

    feat: added Reservation component

--- app/components/Reservations/AddReservation/AddReservationPanel/Tabs/Reservation/Reservation.tsx
+++ app/components/Reservations/AddReservation/AddReservationPanel/Tabs/Reservation/Reservation.tsx
@@ -1,7 +1,29 @@
+'use client';
 import React from 'react';
-
+import 'react-datepicker/dist/react-datepicker.css';
+import DatePicker from './DatePicker/DatePicker';
+import NumOfNights from './NumOfNights/NumOfNights';
+import BookingStatus from './BookingStatus/BookingStatus';
+import NumOfGuests from './NumOfGuests/NumOfGuests';
+import Header from './Header/Header';
 const Reservation = () => {
-  return <div>Reservation</div>;
+  return (
+    <>
+      <div className="flex flex-col gap-2">
+        <Header title="Termin" />
+        <DatePicker />
+        <NumOfNights />
+      </div>
+      <div className="flex flex-col gap-2">
+        <Header title="Status" />
+        <BookingStatus />
+      </div>
+      <div className="flex flex-col gap-2">
+        <Header title="Liczba gości" />
+        <NumOfGuests />
+      </div>
+    </>
+  );
 };
 
 export default Reservation;
