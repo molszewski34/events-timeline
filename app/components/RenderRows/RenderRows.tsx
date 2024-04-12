f13470d5f4e9a7775633edc1ac02a051652e2392
molszewski34 <m.olszewski341@gmail.com>
2024-04-12 08:48:27 +0200

    feat: added feature which allows to move timeline by 7 using buttons

--- app/components/RenderRows/RenderRows.tsx
+++ app/components/RenderRows/RenderRows.tsx
@@ -6,6 +6,10 @@ import {
   startOfMonth,
   endOfMonth,
   differenceInDays,
+  format,
+  isWeekend,
+  isToday,
+  isSameDay,
 } from 'date-fns';
 import { pl } from 'date-fns/locale';
 import { Room } from './types';
@@ -18,7 +22,17 @@ export const RenderRows = () => {
   const { currentDate, daysToShow, setDaysToShow } = useCalendarContext();
   const { setSelectedStartDate, setSelectedEndDate } =
     useAddReservationContext();
+  // const dateFormat = 'EEEEEE dd';
+
+  const [startDate, setStartDate] = useState<Date>(
+    startOfWeek(startOfMonth(currentDate), { locale: pl })
+  );
+
   const dateFormat = 'EEEEEE dd';
+  const days: JSX.Element[] = [];
+
+  let endDate: Date = addDays(startDate, daysToShow);
+  let currentDateIterator = startDate;
 
   const [selectedButton, setSelectedButton] = useState<{
     room: Room | null;
@@ -28,6 +42,42 @@ export const RenderRows = () => {
   const handleButtonClick = (room: Room, timestamp: number) => {
     setSelectedButton({ room, timestamp });
   };
+
+  const handlePrevWeek = () => {
+    setStartDate((prevStartDate) => addDays(prevStartDate, -7));
+  };
+
+  const handleNextWeek = () => {
+    setStartDate((prevStartDate) => addDays(prevStartDate, 7));
+  };
+
+  while (currentDateIterator <= endDate) {
+    const words = format(currentDateIterator, dateFormat).split(' ');
+    const isWeekendDay = isWeekend(currentDateIterator);
+    days.push(
+      <div
+        key={currentDateIterator.toString()}
+        className={`w-[50px] h-[50px] px-[15px] flex flex-col text-sm text-center justify-between ${
+          isToday(currentDateIterator) ? 'today' : ''
+        } ${isSameDay(currentDateIterator, currentDate) ? 'bg-gray-00' : ''} ${
+          isWeekendDay
+            ? 'bg-gray-300 border border-white border-t-2 border-t-black py-[3px] font-bold'
+            : 'py-[5px]'
+        }`}
+        {...(currentDateIterator === currentDate
+          ? { 'aria-current': 'date' }
+          : {})}
+      >
+        <div className="text-sm" style={{ fontSize: 'small' }}>
+          {words[0]}
+        </div>
+        <div className="text-lg" style={{ fontSize: 'large' }}>
+          {words[1]}
+        </div>
+      </div>
+    );
+    currentDateIterator = addDays(currentDateIterator, 1);
+  }
 
   useEffect(() => {
     const handleResize = () => {
@@ -47,7 +97,8 @@ export const RenderRows = () => {
 
   const rows = rooms.map((room: Room) => {
     const days: JSX.Element[] = [];
-    let startDate = startOfWeek(startOfMonth(currentDate), { locale: pl });
+    console.log(currentDateIterator);
+    // let startDate = startOfWeek(startOfMonth(currentDate), { locale: pl });
     let endDate = endOfWeek(endOfMonth(currentDate), { locale: pl });
 
     const roomEventStartDates = room.events.map((event) =>
@@ -57,10 +108,10 @@ export const RenderRows = () => {
       new Date(event.end).setHours(0, 0, 0, 0)
     );
 
-    endDate = addDays(startDate, daysToShow);
+    endDate = addDays(currentDateIterator, daysToShow);
 
-    while (startDate <= endDate) {
-      const currentDateTimestamp = startDate.getTime();
+    while (currentDateIterator <= endDate) {
+      const currentDateTimestamp = currentDateIterator.getTime();
       let eventDuration = '';
       let eventOverlaySize = '';
 
@@ -74,12 +125,12 @@ export const RenderRows = () => {
         const daysDifference = differenceInDays(end, start);
         eventDuration = `(${daysDifference} dni)`;
         eventOverlaySize = `${(daysDifference + 1) * 50}px`;
-        console.log(eventOverlaySize);
+        // console.log(eventOverlaySize);
       }
 
       days.push(
         <button
-          key={`${room.id}-${startDate.toString()}`}
+          key={`${room.id}-${currentDateIterator.toString()}`}
           className={
             ' flex flex-col flex-wrap relative w-[50px] h-[50px] bg-gray-100 border border-white '
           }
@@ -106,7 +157,7 @@ export const RenderRows = () => {
           )}
         </button>
       );
-      startDate = addDays(startDate, 1);
+      currentDateIterator = addDays(currentDateIterator, 1);
     }
 
     return (
@@ -116,5 +167,14 @@ export const RenderRows = () => {
     );
   });
 
-  return rows;
+  return (
+    <div className="flex flex-col">
+      <>
+        <button onClick={handlePrevWeek}>Poprzedni tydzień</button>
+        <button onClick={handleNextWeek}>Następny tydzień</button>
+      </>
+      <div className="flex"> {days}</div>
+      <div className="flex flex-col"> {rows}</div>
+    </div>
+  );
 };
