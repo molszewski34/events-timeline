3424a01d3b3012dfa9ecb8042254e4a4385de6d3
molszewski34 <m.olszewski341@gmail.com>
2024-04-14 17:33:03 +0200

    style: added overflow-auto-y

--- app/components/Reservations/AddReservation/AddReservationPanel/AddReservationPanel.tsx
+++ app/components/Reservations/AddReservation/AddReservationPanel/AddReservationPanel.tsx
@@ -9,7 +9,7 @@ const AddReservationPanel = () => {
   const { openAddReservationPanel, setOpenAddReservationPanel } =
     useAddReservationContext();
   return (
-    <main className="absolute top-0 bottom-0 right-0 left-0 bg-white z-[999] p-2 flex flex-col gap-4">
+    <main className="absolute top-0 bottom-0 right-0 left-0 bg-white z-[999] p-2 h-screen flex flex-col gap-4 overflow-y-auto">
       <Header />
       <TabsComponent />
     </main>
