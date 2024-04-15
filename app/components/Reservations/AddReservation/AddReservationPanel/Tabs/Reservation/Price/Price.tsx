2abfa0f71fae53c6a60aaee877d504395bbb3f54
molszewski34 <m.olszewski341@gmail.com>
2024-04-15 10:36:41 +0200

    feat: added useEffect hook to sum dependencies on change

--- app/components/Reservations/AddReservation/AddReservationPanel/Tabs/Reservation/Price/Price.tsx
+++ app/components/Reservations/AddReservation/AddReservationPanel/Tabs/Reservation/Price/Price.tsx
@@ -1,23 +1,36 @@
-import React from 'react';
-import { rooms } from '@/app/data/roomsData';
+'use client';
+import React, { useState, useEffect } from 'react';
+import { useAddReservationContext } from '@/app/contexts/AddReservation/AddReservationProvider';
 const Price = () => {
+  const { daysBetween, selectedRoom, totalNumOfGuests } =
+    useAddReservationContext();
+
+  const [price, setPrice] = useState(0);
+  useEffect(() => {
+    setPrice(daysBetween * totalNumOfGuests * selectedRoom.roomPrice);
+  }, [daysBetween, totalNumOfGuests, selectedRoom]);
+
   return (
     <div className="">
       <div className="flex flex-col gap-2">
         <h2 className="text-sm font-bold text-gray-400">Finalna cena</h2>
         <form className="border border-gray-300 flex justify-between items-center gap-2 rounded-sm">
-          <input className=" w-full pl-2" type="text" />
+          <input className=" w-full pl-2" value={price} type="text" />
           <p className="text-gray-400">PLN</p>
-          <button className="w-8  rounded-sm rounded-l-none material-icon p-2 bg-green-400 text-xl text-white text-center">
+          <button className="w-12  rounded-sm rounded-l-none material-icon p-2 bg-green-400 text-xl text-white text-center">
             calculate
           </button>
         </form>
       </div>
       <div className="flex flex-col gap-2">
-        <h2 className="text-sm font-bold text-gray-400">Finalna cena</h2>
-        <form className="border border-gray-300 flex justify-between items-center gap-2 rounded-sm p-2">
-          <input className=" w-full pl-2" type="text" />
-          <p className="text-gray-400 pr-8">PLN</p>
+        <h2 className="text-sm font-bold text-gray-400">Cena za dzień</h2>
+        <form className="border border-gray-300 flex justify-between items-center gap-2 rounded-sm py-2">
+          <input
+            className=" w-full pl-2"
+            value={selectedRoom.roomPrice}
+            type="text"
+          />
+          <p className="text-gray-400 pr-12">PLN</p>
         </form>
       </div>
     </div>
