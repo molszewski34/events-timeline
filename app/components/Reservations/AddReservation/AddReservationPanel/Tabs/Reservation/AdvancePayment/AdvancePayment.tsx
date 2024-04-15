a783d9eb2a850801ac8993b47ebe89c12ad1c949
molszewski34 <m.olszewski341@gmail.com>
2024-04-15 20:29:16 +0200

    feat: added feature which allows to set advancePayment by input

--- app/components/Reservations/AddReservation/AddReservationPanel/Tabs/Reservation/AdvancePayment/AdvancePayment.tsx
+++ app/components/Reservations/AddReservation/AddReservationPanel/Tabs/Reservation/AdvancePayment/AdvancePayment.tsx
@@ -1,11 +1,18 @@
-import React from 'react';
-import { useState } from 'react';
-const AdvancePayment = () => {
-  const [isOpen, setIsOpen] = useState(false);
-  const [advancePayment, setAdvancePayment] = useState();
+import React, { useState, ChangeEvent } from 'react';
+
+const AdvancePayment: React.FC = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [advancePayment, setAdvancePayment] = useState<string>('');
+
+  const handleAdvancePaymentChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setAdvancePayment(event.target.value);
+  };
+
+  console.log(advancePayment);
+
   return (
     <section className="flex flex-col gap-2">
-      <div className="flex justify-between items-center gap-2 ">
+      <div className="flex justify-between items-center gap-2">
         <h2 className="font-semibold text-gray-400">Zaliczka</h2>
         <hr className="bg-gray-70000 w-full" />
         <button
@@ -18,10 +25,11 @@ const AdvancePayment = () => {
       </div>
       {isOpen && (
         <div className="flex flex-col gap-2">
-          <form className="border border-gray-300 flex justify-between items-center gap-2 rounded-sm ">
+          <form className="border border-gray-300 flex justify-between items-center gap-2 rounded-sm">
             <input
-              className=" w-full pl-2 py-2"
+              className="w-full pl-2 py-2"
               value={advancePayment}
+              onChange={handleAdvancePaymentChange}
               type="text"
               aria-label="Cena za dzień"
             />
