6ae339995db5064d76dea66b3a4c0a349437826d
molszewski34 <m.olszewski341@gmail.com>
2024-04-16 09:51:17 +0200

    refactor, feat: changed forms, header into component, added checbox function

--- app/components/Reservations/AddReservation/AddReservationPanel/Tabs/Reservation/PaymentOnPlace/PaymentOnPlace.tsx
+++ app/components/Reservations/AddReservation/AddReservationPanel/Tabs/Reservation/PaymentOnPlace/PaymentOnPlace.tsx
@@ -1,6 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Header from './Header';
+import Form from './Form';
+import { useAddReservationContext } from '@/app/contexts/AddReservation/AddReservationProvider';
 
 const PaymentOnPlace = () => {
+  const {
+    paymentOnPlace,
+    setPaymentOnPlace,
+    deposit,
+    setDeposit,
+    localTax,
+    setLocalTax,
+    includedTax,
+    setIncludeTax,
+  } = useAddReservationContext();
+
   return (
     <main>
       <div className="flex justify-between items-center gap-1 flex-nowrap ">
@@ -9,36 +23,27 @@ const PaymentOnPlace = () => {
         </h2>
         <hr className="bg-gray-70000 w-full" />
       </div>
-      <div className="flex flex-col gap-2">
-        <form className="border border-gray-300 flex justify-between items-center gap-2 rounded-sm bg-gray-200">
-          <input className="w-full pl-2 py-2 bg-gray-200" type="text" />
-          <p className="text-gray-400 pr-12">PLN</p>
-        </form>
-      </div>
+      <Form value={paymentOnPlace} onChange={setPaymentOnPlace} />
       <div className="flex justify-between items-center gap-1 flex-nowrap ">
-        <h2 className="font-semibold text-gray-400 w-full text-sm">Depozyt</h2>
+        <Header title={'Depozyt'} />
       </div>
-      <div className="flex flex-col gap-2">
-        <form className="border border-gray-300 flex justify-between items-center gap-2 rounded-sm ">
-          <input className="w-full pl-2 py-2 " type="text" />
-          <p className="text-gray-400 pr-12">PLN</p>
-        </form>
-      </div>
+      <Form value={deposit} onChange={setDeposit} />
       <div className="flex justify-between items-center gap-1 flex-nowrap ">
-        <h2 className="font-semibold text-gray-400 w-full text-sm">
-          Lokalny podatek
-        </h2>
+        <Header title={'Lokalny podatek'} />
       </div>
       <div className="flex flex-col gap-2">
-        <form className="border border-gray-300 flex justify-between items-center gap-2 rounded-sm b">
-          <input className="w-full pl-2 py-2 " type="text" />
-          <p className="text-gray-400 pr-12">PLN</p>
-        </form>
+        <Form value={localTax} onChange={setLocalTax} />
         <label
           className="text-xs flex items-center gap-2 justify-end"
           htmlFor="tax"
         >
-          <input type="checkbox" name="" id="tax" />
+          <input
+            type="checkbox"
+            name=""
+            id="tax"
+            checked={includedTax}
+            onChange={(e) => setIncludeTax(e.target.checked)}
+          />
           Wlicz podatek do ceny
         </label>
       </div>
