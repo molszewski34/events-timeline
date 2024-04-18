d0107e28a2d9e8047d17c95b9826ba14bc0b828f
molszewski34 <m.olszewski341@gmail.com>
2024-04-18 08:59:00 +0200

    refactor: moved statuses to data file

---
+++ app/components/Reservations/AddReservation/AddReservationPanel/Tabs/Reservation/BookingStatus/data.ts
@@ -0,0 +1,7 @@
+export const statuses = [
+  { name: 'Brak płatności', color: '#f87171' },
+  { name: 'Wpłacono zaliczke', color: '#fde68a' },
+  { name: 'Zapłacono', color: '#a3e635' },
+  { name: 'Anulowano', color: '#a78bfa' },
+  { name: 'Niedostępny', color: '#a3a3a3' },
+];
