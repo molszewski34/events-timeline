d4f47542b40bae809b188c0d98e84aa7ad7f1619
molszewski34 <m.olszewski341@gmail.com>
2024-04-20 17:39:38 +0200

    feat: added new interface and states to formData

--- app/contexts/AddReservation/AddReservationProvider.tsx
+++ app/contexts/AddReservation/AddReservationProvider.tsx
@@ -1,8 +1,9 @@
 'use client';
-import { AgeOfKids } from './types';
+import { AgeOfKids, FormData } from './types';
 import { createContext, useContext, useState } from 'react';
 import { Room } from '@/app/components/RenderRows/types';
 import { rooms } from '@/app/data/roomsData';
+import { statuses } from '@/app/components/Reservations/AddReservation/AddReservationPanel/Tabs/Reservation/BookingStatus/data';
 export const AddReservation = createContext<any>(undefined);
 export function AddReservationWrapper({
   children,
@@ -15,14 +16,31 @@ export function AddReservationWrapper({
   const [daysBetween, setDaysBetween] = useState(0);
   const [numOfKids, setNumOfKids] = useState<number>(0);
   const [ageOfKids, setAgeOfKids] = useState<AgeOfKids>({});
-  const [selectedRoom, setSelectedroom] = useState<Room | null>(rooms[0]);
+
+  const [selectedRoom, setSelectedroom] = useState<Room | null>();
   const [roomListOpen, setRoomListOpen] = useState(false);
   const [totalNumOfGuests, setNumOfGuests] = useState<number>(0);
   const [advancePayment, setAdvancePayment] = useState<string>('');
   const [paymentOnPlace, setPaymentOnPlace] = useState<number>(0);
   const [deposit, setDeposit] = useState<number>(0);
-  const [localTax, setLocalTax] = useState<number>(0.85);
-  const [includedTax, setIncludeTax] = useState(false);
+  const [localTax, setLocalTax] = useState<number>(0.085);
+  const [price, setPrice] = useState(0);
+  const [tax, setTax] = useState<number>(0);
+  const [includedTax, setIncludedTax] = useState(false);
+  const [selectedRoomId, setSelectedRoomId] = useState('');
+  const [formData, setFormData] = useState<FormData>({
+    selectedStartDate: new Date(),
+    selectedEndDate: new Date(),
+    selectedStatus: statuses[0],
+    selectedRoom: rooms[0],
+    numOfAdults: 0,
+    numOfKids: 0,
+    advancePayment: '',
+    deposit: '',
+    paymentOnPlace: '',
+    localTax: 0.085,
+  });
+
   return (
     <AddReservation.Provider
       value={{
@@ -53,7 +71,15 @@ export function AddReservationWrapper({
         localTax,
         setLocalTax,
         includedTax,
-        setIncludeTax,
+        setIncludedTax,
+        formData,
+        setFormData,
+        selectedRoomId,
+        setSelectedRoomId,
+        price,
+        setPrice,
+        tax,
+        setTax,
       }}
     >
       {children}
