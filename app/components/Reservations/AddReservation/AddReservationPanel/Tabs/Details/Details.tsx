76b9d8ce40c010400fb260ea56018e78c85c48cc
molszewski34 <m.olszewski341@gmail.com>
2024-04-24 09:07:34 +0200

    feat: created Details component and added child components

--- app/components/Reservations/AddReservation/AddReservationPanel/Tabs/Details/Details.tsx
+++ app/components/Reservations/AddReservation/AddReservationPanel/Tabs/Details/Details.tsx
@@ -1,7 +1,30 @@
 import React from 'react';
+import SectionHeader from '../../Headers/SectionHeader';
+import Label from '../../Headers/Label';
+import Notes from './Notes/Notes';
+import Field from '../Client/Field/Field';
+import Registration from './Registration/Registration';
+import Boarding from './Boarding/Boarding';
 
 const Details = () => {
-  return <div>Details</div>;
+  return (
+    <main className="flex flex-col gap-2">
+      <section className="flex flex-col gap-2">
+        <SectionHeader title="Pozostałe" />
+        <Label title="Notatki" />
+        <Notes />
+      </section>
+
+      <Registration />
+      <Boarding />
+      <Field
+        formDataKey="passCode"
+        label="Kod do drzwi"
+        type="text"
+        placeholder=""
+      />
+    </main>
+  );
 };
 
 export default Details;
