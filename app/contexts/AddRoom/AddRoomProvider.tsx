fe407b9fe61da80e2cc20a1af38542459d032ef6
molszewski34 <m.olszewski341@gmail.com>
2024-05-10 10:53:47 +0200

    feat: created new initial provider for AddRoomPanel and components

---
+++ app/contexts/AddRoom/AddRoomProvider.tsx
@@ -0,0 +1,58 @@
+'use client';
+
+import { createContext, useContext, useState } from 'react';
+import { RoomFormData } from './types';
+export const AddRoom = createContext<any>(undefined);
+
+export function AddRoomWrapper({ children }: { children: React.ReactNode }) {
+  const [roomFormData, setRoomFormData] = useState<RoomFormData>({
+    roomName: '',
+    roomGuests: 0,
+    roomPrice: 0,
+    roomType: 'Pokój',
+    roomTypeIcon: 'bed',
+    roomDetails: '',
+    roomExtras: '',
+    roomArea: '',
+    roomNumOfPersons: 2,
+    roomAdditionalPersons: 0,
+    roomNumOfSingleBeds: 2,
+    roomNumOfDoubleBeds: 0,
+    roomColor: '',
+    roomCountry: {
+      name: 'Polska',
+      file_url: '//upload.wikimedia.org/wikipedia/en/1/12/Flag_of_Poland.svg',
+    },
+    roomAdress: '',
+    roomPostCode: '',
+    roomCity: '',
+  });
+
+  console.log(roomFormData);
+
+  console.log(roomFormData.roomType);
+  const [openAddRoom, setOpenAddRoom] = useState(true);
+  const [roomListOpen, setRoomListOpen] = useState(false);
+  const [selectedRoomType, setSelectedRoomType] = useState(false);
+
+  return (
+    <AddRoom.Provider
+      value={{
+        roomFormData,
+        setRoomFormData,
+        openAddRoom,
+        setOpenAddRoom,
+        roomListOpen,
+        setRoomListOpen,
+        selectedRoomType,
+        setSelectedRoomType,
+      }}
+    >
+      {children}
+    </AddRoom.Provider>
+  );
+}
+
+export function useAddRoomContext() {
+  return useContext(AddRoom);
+}
---
+++ app/contexts/AddRoom/types.ts
@@ -0,0 +1,24 @@
+export interface RoomFormData {
+  roomName: string;
+  roomGuests: number;
+  roomPrice: number;
+  roomType: string;
+  roomTypeIcon: string;
+  roomDetails: string;
+  roomExtras: string;
+  roomArea: string;
+  roomNumOfPersons: number;
+  roomAdditionalPersons: number;
+  roomNumOfSingleBeds: number;
+  roomNumOfDoubleBeds: number;
+  roomColor: string;
+  roomCountry: RoomCountryTypes;
+  roomAdress: string;
+  roomPostCode: string;
+  roomCity: string;
+}
+
+interface RoomCountryTypes {
+  name: string;
+  file_url: string;
+}
