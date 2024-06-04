604ecd756fbcd674509b4b32a17f258c00c10ecd
molszewski34 <m.olszewski341@gmail.com>
2024-06-04 09:49:33 +0200

    feat: added con render.
    If modal is open in editing mode then diffrent button apear

--- app/components/Reservations/AddReservation/AddReservationPanel/Footer/Footer.tsx
+++ app/components/Reservations/AddReservation/AddReservationPanel/Footer/Footer.tsx
@@ -2,14 +2,17 @@ import React, { useContext, useState } from 'react';
 import { Button } from '@/app/components/ui/button';
 import { useAddReservationContext } from '@/app/contexts/AddReservation/AddReservationProvider';
 import AddReservationSubmitBtn from '../../AddReservationSubmitBtn/AddReservationSubmitBtn';
+import { useCalendarContext } from '@/app/contexts/Calendar/CalendarProvider';
+import UpdateReservationButton from '../../UpdateReservationButton/UpdateReservationButton';
 
 const Footer: React.FC = () => {
+  const { isEditing } = useCalendarContext();
   return (
     <div className="flex items-center">
       <button className="flex gap-1 bg-gray-300   w-full items-center justify-center py-1 rounded-sm ">
         Zamknij
       </button>
-      <AddReservationSubmitBtn />
+      {isEditing ? <UpdateReservationButton /> : <AddReservationSubmitBtn />}
     </div>
   );
 };
