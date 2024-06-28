2d81da9098cf53b04e89b7673fddb35b3dfb1d17
molszewski34 <m.olszewski341@gmail.com>
2024-06-28 09:27:46 +0200

    feat: added feat to reset form sates and setIsEditing to false

--- app/components/Rooms/AddRoomPanel/Headers/Header.tsx
+++ app/components/Rooms/AddRoomPanel/Headers/Header.tsx
@@ -3,20 +3,24 @@ import React from 'react';
 import { Button } from '@/app/components/ui/button';
 import { useAddRoomContext } from '@/app/contexts/AddRoom/AddRoomProvider';
 import { useCalendarContext } from '@/app/contexts/Calendar/CalendarProvider';
+import { initialRoomFormData } from '@/app/contexts/AddRoom/initialRoomFormData';
 const Header = () => {
-  const { setOpenAddRoom } = useAddRoomContext();
-  const { setOverlay } = useCalendarContext();
-
+  const { setOpenAddRoom, setRoomFormData } = useAddRoomContext();
+  const { setOverlay, setIsEditing } = useCalendarContext();
+  const resetRoomFormData = () => {
+    setRoomFormData(initialRoomFormData);
+  };
   return (
     <header className="flex flex-col  w-full bg-white ">
       <div className="flex justify-between items-center">
         <h1 className="font-semibold text-sm text-gray-500">Dodaj Pokój</h1>
         <button
           className="material-icon text-gray-700 rounded-full text-lg w-9 h-9 text-right"
-          // variant={'ghost'}
           onClick={() => {
             setOpenAddRoom(false);
             setOverlay(false);
+            resetRoomFormData();
+            setIsEditing(false);
           }}
         >
           close
