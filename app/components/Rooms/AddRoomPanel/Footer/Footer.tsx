168102c0b173b1d77b43b3d7ec9caec95f2ea3b9
molszewski34 <m.olszewski341@gmail.com>
2024-07-02 08:13:39 +0200

    feat: added component and added con render

--- app/components/Rooms/AddRoomPanel/Footer/Footer.tsx
+++ app/components/Rooms/AddRoomPanel/Footer/Footer.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import AddRoomSubmitBtn from '../AddRoomSubmitBtn/AddRoomSubmitBtn';
+import { useCalendarContext } from '@/app/contexts/Calendar/CalendarProvider';
+import UpdateRoomButton from '../../UpdateRoomButton/UpdateRoomButton';
 
 const Footer = () => {
+  const { isEditing } = useCalendarContext();
+
   return (
     <div className="flex items-center">
       <button className="flex gap-1 bg-gray-200 hover:bg-gray-300 text-black w-full items-center justify-center py-1 rounded-sm">
         Zamknij
       </button>
-      <AddRoomSubmitBtn />
+      {isEditing ? <UpdateRoomButton /> : <AddRoomSubmitBtn />}
     </div>
   );
 };
