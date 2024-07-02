5c902f42c77e15e7c86451a9e93757dc14a4642f
molszewski34 <m.olszewski341@gmail.com>
2024-07-02 10:27:45 +0200

    feat: added conditional open

--- app/components/Navigation/SearchBar/SearchBar.tsx
+++ app/components/Navigation/SearchBar/SearchBar.tsx
@@ -1,10 +1,21 @@
+'use client';
 import React from 'react';
+import { useCalendarContext } from '@/app/contexts/Calendar/CalendarProvider';
 
 const SearchBar = () => {
+  const { openSearchBar } = useCalendarContext();
   return (
-    <main className="absolute top-0 left-0 right-0 z-[60] bg-white w-full ">
-      <input className="w-full h-16 pl-8" type="text" placeholder="Szukaj" />
-    </main>
+    <>
+      {openSearchBar && (
+        <main className="absolute top-0 left-0 right-0 z-[60] bg-white w-full ">
+          <input
+            className="w-full h-16 pl-8"
+            type="text"
+            placeholder="Szukaj"
+          />
+        </main>
+      )}
+    </>
   );
 };
 
