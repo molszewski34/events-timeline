e80effa5072f065c171790beef163d4f8d01dd75
molszewski34 <m.olszewski341@gmail.com>
2024-07-02 09:50:34 +0200

    feat: init

---
+++ app/components/Navigation/SearchBtn/SearchBtn.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SearchBtn = () => {
+  return (
+    <button className="w-11">
+      <i className="text-2xl">search</i>
+    </button>
+  );
+};
+
+export default SearchBtn;
