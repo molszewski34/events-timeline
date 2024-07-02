18e55f5155238d09c6b65cb18282dd9d07481dee
molszewski34 <m.olszewski341@gmail.com>
2024-07-02 10:28:54 +0200

    feat: added change state to openSearchBar and overlaySearchBar

--- app/components/Navigation/SearchBtn/SearchBtn.tsx
+++ app/components/Navigation/SearchBtn/SearchBtn.tsx
@@ -1,8 +1,17 @@
+'use client';
 import React from 'react';
-
+import { useCalendarContext } from '@/app/contexts/Calendar/CalendarProvider';
 const SearchBtn = () => {
+  const { openSearchBar, setOpenSearchBar, setOverlaySearchBar } =
+    useCalendarContext();
   return (
-    <button className="w-11">
+    <button
+      className="w-11"
+      onClick={() => {
+        setOpenSearchBar(true);
+        setOverlaySearchBar(true);
+      }}
+    >
       <i className="text-2xl">search</i>
     </button>
   );
