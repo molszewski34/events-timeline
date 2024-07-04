3a95c7a9f7a5042af743aabd737b8802f6f09fba
molszewski34 <m.olszewski341@gmail.com>
2024-07-04 10:20:32 +0200

    feat: added clear feature

--- app/components/Navigation/SearchBar/OverlaySearchBar/OverlaySearchBar.tsx
+++ app/components/Navigation/SearchBar/OverlaySearchBar/OverlaySearchBar.tsx
@@ -3,8 +3,13 @@ import React from 'react';
 import { useCalendarContext } from '@/app/contexts/Calendar/CalendarProvider';
 
 const OverlaySearchBar = () => {
-  const { overlaySearchBar, setOverlaySearchBar, setOpenSearchBar } =
-    useCalendarContext();
+  const {
+    overlaySearchBar,
+    setOverlaySearchBar,
+    setOpenSearchBar,
+    setSearchQuery,
+    setFilteredReservations,
+  } = useCalendarContext();
   return (
     <div
       className="fixed top-0 bottom-0 left-0 right-0 z-[50] bg-black bg-opacity-50"
@@ -12,6 +17,8 @@ const OverlaySearchBar = () => {
       onClick={() => {
         setOverlaySearchBar(false);
         setOpenSearchBar(false);
+        setSearchQuery('');
+        setFilteredReservations([]);
       }}
     ></div>
   );
