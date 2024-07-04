1e29a2c85fe587b94a5ca14a1e791374274a4216
molszewski34 <m.olszewski341@gmail.com>
2024-07-04 08:52:24 +0200

    feat: added search functionality with 2 conditions

--- app/components/Navigation/SearchBar/SearchBar.tsx
+++ app/components/Navigation/SearchBar/SearchBar.tsx
@@ -1,9 +1,45 @@
 'use client';
 import React from 'react';
 import { useCalendarContext } from '@/app/contexts/Calendar/CalendarProvider';
+import { useQuery } from '@supabase-cache-helpers/postgrest-react-query';
+import { fetchReservations } from '@/app/actions/fetchReservations';
+import useSupabaseBrowser from '@/utils/supabase-browser';
+import NoResults from './SearchResult/NoResults';
+import SearchResult from './SearchResult/SearchResult';
 
-const SearchBar = () => {
-  const { openSearchBar } = useCalendarContext();
+const SearchBar = ({ id }: { id: string }) => {
+  const supabase = useSupabaseBrowser();
+
+  const { data: reservations } = useQuery(fetchReservations(supabase, id));
+
+  const {
+    openSearchBar,
+    searchQuery,
+    setSearchQuery,
+    filteredReservations,
+    setFilteredReservations,
+  } = useCalendarContext();
+  const handleSearch = (e) => {
+    const query = e.target.value.toLowerCase();
+    setSearchQuery(query);
+
+    const filtered = reservations.filter((reservation) => {
+      const { phone, email, id, main_guest } = reservation;
+
+      return (
+        phone.toLowerCase().includes(query) ||
+        email.toLowerCase().includes(query) ||
+        id.toString().toLowerCase().includes(query) ||
+        main_guest.toLowerCase().includes(query)
+      );
+    });
+
+    if (query == '') {
+      setFilteredReservations([]);
+    } else setFilteredReservations(filtered);
+  };
+
+  console.log(filteredReservations);
   return (
     <>
       {openSearchBar && (
@@ -12,7 +48,11 @@ const SearchBar = () => {
             className="w-full h-16 pl-8"
             type="text"
             placeholder="Szukaj"
+            value={searchQuery}
+            onChange={handleSearch}
           />
+          {searchQuery && filteredReservations.length === 0 && <NoResults />}{' '}
+          {filteredReservations.length > 0 && <SearchResult />}
         </main>
       )}
     </>
