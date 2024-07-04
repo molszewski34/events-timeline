dc1e9220a6c896aefb40261a0c7779ca33669c04
molszewski34 <m.olszewski341@gmail.com>
2024-07-04 08:52:56 +0200

    feat: created condition for SearchBar

---
+++ app/components/Navigation/SearchBar/SearchResult/NoResults.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NoResults = () => {
+  return (
+    <div className="bg-gray-300 text-xs">
+      Brak wyników pasujących do kryteriów wyszukiwania
+    </div>
+  );
+};
+
+export default NoResults;
