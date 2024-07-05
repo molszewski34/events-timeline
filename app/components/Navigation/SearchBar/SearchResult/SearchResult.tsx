b611c56047a707eed17b59a7e5ab5ac455c6deda
molszewski34 <m.olszewski341@gmail.com>
2024-07-05 09:56:39 +0200

    refactor: changed path

--- app/components/Navigation/SearchBar/SearchResult/SearchResult.tsx
+++ app/components/Navigation/SearchBar/SearchResult/SearchResult.tsx
@@ -1,7 +1,7 @@
 import { useCallback } from 'react';
 import { useCalendarContext } from '@/app/contexts/Calendar/CalendarProvider';
 import { Reservation } from '@/app/components/RenderRows/types';
-import useHandleSetFormData from '@/app/hooks/handleSetFormData';
+import useHandleSetFormData from '@/app/hooks/SearchResults/handleSetFormData';
 import { useAddReservationContext } from '@/app/contexts/AddReservation/AddReservationProvider';
 
 const SearchResult = () => {
@@ -20,44 +20,7 @@ const SearchResult = () => {
     openAddReservationPanel,
   } = useAddReservationContext();
 
-  const handleSetFormData = useCallback(
-    (query: Reservation) => {
-      setFormData((prevData: FormData) => ({
-        ...prevData,
-        selectedReservationId: query.id,
-        selectedStartDate: query.selected_start_date,
-        selectedEndDate: query.selected_end_date,
-        selectedStatus: query.selected_status,
-        numOfAdults: query.num_of_adults,
-        numOfKids: query.num_of_kids,
-        advancePayment: query.advance_payment,
-        deposit: query.deposit,
-        paymentOnPlace: query.payment_on_place,
-        localTax: query.local_tax,
-        mainGuest: query.main_guest,
-        phone: query.phone,
-        email: query.email,
-        houseNumber: query.house_number,
-        apartmentNumber: query.apartment_number,
-        city: query.city,
-        postCode: query.post_code,
-        country: query.country,
-        passport: query.passport,
-        company: query.company,
-        companyStreet: query.company_street,
-        companyCity: query.company_city,
-        companyPostCode: query.company_post_code,
-        companyCountry: query.company_country,
-        companyNip: query.company_nip,
-        notes: query.notes,
-        passCode: query.pass_code,
-        registration: query.registration,
-        boarding: query.boarding,
-      }));
-    },
-    [setFormData]
-  );
-
+  const { handleSetFormData } = useHandleSetFormData();
   return (
     <main>
       <div className="flex justify-between py-2 px-4 text-xs bg-gray-200">
