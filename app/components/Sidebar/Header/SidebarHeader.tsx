b97d0b412ee7463c76fb72338ef7f25310be2b20
molszewski34 <m.olszewski341@gmail.com>
2024-07-09 09:29:25 +0200

    feat: added button functionality to open sidebar

--- app/components/Sidebar/Header/SidebarHeader.tsx
+++ app/components/Sidebar/Header/SidebarHeader.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import { useSidebarContext } from '@/app/contexts/Sidebar/SidebarProvider';
 
 const SidebarHeader = () => {
+  const { openSidebar, setOpenSidebar } = useSidebarContext();
+
   return (
-    <div className="flex items-center px-4 py-4 w-60 border border-gray-100 gap-2">
-      <i className="text-xl">close</i>
-      <div className="flex text-2xl font-bold">
+    <div className="flex items-center px-4 py-4 border border-gray-100  w-full">
+      <button>
+        <i className="text-xl p-2" onClick={() => setOpenSidebar(false)}>
+          close
+        </i>
+      </button>
+      <div className="flex text-2xl font-bold ">
         <p>Events</p>
         <p className="text-green-600">Timeline</p>
       </div>
