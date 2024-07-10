d1119a605cab38fc8615be25b29a9bb818d17215
molszewski34 <m.olszewski341@gmail.com>
2024-07-10 11:21:44 +0200

    feat: added reservations list based on selected month

--- app/booking/page.tsx
+++ app/booking/page.tsx
@@ -1,13 +1,33 @@
 'use client';
 
 import { useState, useEffect } from 'react';
-import { format, addMonths, addYears, subMonths, subYears } from 'date-fns';
-export default function Home() {
+import {
+  format,
+  addMonths,
+  addYears,
+  subMonths,
+  subYears,
+  getMonth,
+  getYear,
+} from 'date-fns';
+import { Database } from '@/types/supabase';
+import { useQuery } from '@supabase-cache-helpers/postgrest-react-query';
+import { fetchReservations } from '@/app/actions/fetchReservations';
+import useSupabaseBrowser from '@/utils/supabase-browser';
+
+export default function Home({ id }: { id: string }) {
   const [date, setDate] = useState('');
   const [month, setMonth] = useState('');
   const [year, setYear] = useState('');
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [currentDate, setCurrentDate] = useState(new Date());
+  const [filteredReservations, setFilteredReservations] = useState([]);
+  const supabase = useSupabaseBrowser();
+
+  const { data: reservations } = useQuery(fetchReservations(supabase, id));
+
+  type Reservation = Database['public']['Tables']['reservations']['Row'];
+
   useEffect(() => {
     const formattedMonth = format(currentDate, 'MMM');
     const formattedYear = format(currentDate, 'yyyy');
@@ -15,6 +35,17 @@ export default function Home() {
     setMonth(formattedMonth);
     setYear(formattedYear);
     setDate(formattedDate);
+
+    const filtered = reservations.filter((reservation) => {
+      const reservationMonth = getMonth(reservation.selected_start_date);
+      const reservationYear = getYear(reservation.selected_start_date);
+      return (
+        reservationMonth === getMonth(currentDate) &&
+        reservationYear === getYear(currentDate)
+      );
+    });
+
+    setFilteredReservations(filtered);
   }, [currentDate]);
 
   const openModal = () => {
@@ -42,7 +73,7 @@ export default function Home() {
   };
 
   return (
-    <div className="flex items-center justify-center  bg-gray-100  mt-6">
+    <div className="flex flex-col items-center justify-center  bg-gray-100  mt-6">
       <div className="p-6 bg-white rounded shadow-md w-full mx-3">
         <label
           className="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm text-xs"
@@ -52,6 +83,20 @@ export default function Home() {
           <input type="text" id="date" value={date} readOnly />
           <i>calendar_today</i>
         </label>
+      </div>
+      <div className="mt-4">
+        <h3 className="text-lg font-medium">Filtered Reservations</h3>
+        <ul className="mt-2">
+          {filteredReservations.length > 0 ? (
+            filteredReservations.map((reservation) => (
+              <li key={reservation.id}>
+                {format(reservation.selected_start_date, 'MMMM d, yyyy')}
+              </li>
+            ))
+          ) : (
+            <li>No reservations for {date}</li>
+          )}
+        </ul>
       </div>
       {isModalOpen && (
         <div className="fixed inset-0 flex items-center justify-center z-50">
