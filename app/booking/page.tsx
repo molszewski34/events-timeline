c60cbcedf226ff994ab8947cfd03b50072609116
molszewski34 <m.olszewski341@gmail.com>
2024-07-11 11:18:35 +0200

    feat: added tabs Najnowsze and Anulowane.
    Anulowane displays canceled reservations based on status stored in db

--- app/booking/page.tsx
+++ app/booking/page.tsx
@@ -25,6 +25,7 @@ import DeleteConfirmation from '../components/Reservations/AddReservation/Delete
 import OverlayDelete from '../components/Reservations/AddReservation/AddReservationPanel/Header/DeleteReservationBtn/OverlayDelete';
 import Overlay from '../components/utils/Overlay';
 import './styles.css';
+
 export default function Home({
   id,
   params,
@@ -40,6 +41,9 @@ export default function Home({
   const [filteredReservations, setFilteredReservations] = useState<
     Database['public']['Tables']['reservations']['Row'][]
   >([]);
+  const [activeTab, setActiveTab] = useState<'Najnowsze' | 'Anulowane'>(
+    'Najnowsze'
+  );
   const supabase = useSupabaseBrowser();
   const { handleSetFormData } = useHandleSetFormData();
 
@@ -111,13 +115,17 @@ export default function Home({
     setCurrentDate(subYears(currentDate, 1));
   };
 
+  const handleTabClick = (tab: 'Najnowsze' | 'Anulowane') => {
+    setActiveTab(tab);
+  };
+
   if (error) {
     return <div>Error loading reservations</div>;
   }
 
   return (
-    <div className="flex flex-col items-center justify-center bg-gray-100 mt-6">
-      <div className="p-6 bg-white rounded shadow-md w-full mx-3 relative">
+    <div className="flex flex-col items-center justify-center bg-gray-100 mt-6 mx-2">
+      <div className="p-2 pb-0 bg-white rounded-t-sm shadow-md w-full mx-3 relative">
         <label
           className="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm text-xs"
           htmlFor="date"
@@ -126,6 +134,28 @@ export default function Home({
           <input type="text" id="date" value={date} readOnly />
           <i>calendar_today</i>
         </label>
+        <div className="flex justify-center">
+          <button
+            onClick={() => handleTabClick('Najnowsze')}
+            className={`py-2 pt-4 w-full ${
+              activeTab === 'Najnowsze'
+                ? 'text-green-600 border-b-2 border-green-600'
+                : 'text-gray-500'
+            }`}
+          >
+            Najnowsze
+          </button>
+          <button
+            onClick={() => handleTabClick('Anulowane')}
+            className={`py-2 px-4 w-full ${
+              activeTab === 'Anulowane'
+                ? 'text-green-600 border-b-2 border-green-600'
+                : 'text-gray-500'
+            }`}
+          >
+            Anulowane
+          </button>
+        </div>
         {isModalOpen && (
           <div className="absolute top-full left-1/2 -translate-x-1/2 w-[180px] flex items-center justify-center z-50 animate-slide-down shadow-md">
             <div className="w-full bg-white shadow-lg flex flex-col">
@@ -157,68 +187,76 @@ export default function Home({
           </div>
         )}
       </div>
-      <div className="mt-4 bg-gray-100 w-full">
-        <ul className="flex flex-col gap-2 mt-2">
+
+      <div className="w-full bg-gray-100">
+        <ul className="flex flex-col gap-2">
           {filteredReservations.length > 0 ? (
-            filteredReservations.map((reservation) => {
-              const startDate = new Date(reservation.selected_start_date);
-              const endDate = new Date(reservation.selected_end_date);
-              return (
-                <div
-                  key={reservation.id}
-                  className="flex flex-col py-2 px-4 gap-2 hover:bg-gray-100 bg-white"
-                  onClick={() => {
-                    handleSetFormData(reservation);
-                    setIsEditing(true);
-                    setOverlaySearchBar(false);
-                    setOpenSearchBar(false);
-                    setOpenAddReservationPanel(true);
-                    setOverlay(true);
-                  }}
-                >
-                  <div className="flex items-center gap-2">
-                    <div
-                      className="h-3 w-3 rounded-full"
-                      style={{
-                        backgroundColor: reservation.selected_status.color,
-                      }}
-                    ></div>
-                    <p className="text-xs font-semibold">
-                      {' '}
-                      {reservation.main_guest}
-                    </p>
-                  </div>
-                  <div className="flex justify-between text-xs ">
-                    <div className="flex gap-2 basis-1/2">
-                      <span className="text-gray-500 font-semibold">Od</span>
-                      <p className="font-semibold">
-                        {startDate.toISOString().substring(0, 10)}
+            filteredReservations
+              .filter((reservation) =>
+                activeTab === 'Najnowsze'
+                  ? reservation.selected_status.name !== 'Anulowana'
+                  : reservation.selected_status.name === 'Anulowana'
+              )
+              .map((reservation) => {
+                const startDate = new Date(reservation.selected_start_date);
+                const endDate = new Date(reservation.selected_end_date);
+                return (
+                  <div
+                    key={reservation.id}
+                    className="flex flex-col py-2 px-4 gap-2 hover:bg-gray-100 bg-white"
+                    onClick={() => {
+                      handleSetFormData(reservation);
+                      setIsEditing(true);
+                      setOverlaySearchBar(false);
+                      setOpenSearchBar(false);
+                      setOpenAddReservationPanel(true);
+                      setOverlay(true);
+                    }}
+                  >
+                    <div className="flex items-center gap-2">
+                      <div
+                        className="h-3 w-3 rounded-full"
+                        style={{
+                          backgroundColor: reservation.selected_status.color,
+                        }}
+                      ></div>
+                      <p className="text-xs font-semibold">
+                        {reservation.main_guest}
                       </p>
                     </div>
-                    <div className="flex gap-2 basis-1/2">
-                      <span className="text-gray-500 font-semibold">Do</span>
-                      <p className="font-semibold">
-                        {endDate.toISOString().substring(0, 10)}
-                      </p>
+                    <div className="flex justify-between text-xs ">
+                      <div className="flex gap-2 basis-1/2">
+                        <span className="text-gray-500 font-semibold">Od</span>
+                        <p className="font-semibold">
+                          {startDate.toISOString().substring(0, 10)}
+                        </p>
+                      </div>
+                      <div className="flex gap-2 basis-1/2">
+                        <span className="text-gray-500 font-semibold">Do</span>
+                        <p className="font-semibold">
+                          {endDate.toISOString().substring(0, 10)}
+                        </p>
+                      </div>
+                    </div>
+                    <div className="flex gap-2 text-xs">
+                      <span className="text-gray-500 font-semibold">
+                        Telefon
+                      </span>
+                      <p className="font-semibold">{reservation.phone}</p>
+                    </div>
+                    <div className="flex gap-2 text-xs">
+                      <span className="text-gray-500 font-semibold">Email</span>
+                      <p className="font-semibold">{reservation.email}</p>
+                    </div>
+                    <div className="flex gap-2 text-xs">
+                      <span className="text-gray-500 font-semibold">ID</span>
+                      <p className="font-semibold">{reservation.id}</p>
                     </div>
                   </div>
-                  <div className="flex gap-2 text-xs">
-                    <span className="text-gray-500 font-semibold">Telefon</span>
-                    <p className="font-semibold"> {reservation.phone}</p>
-                  </div>
-                  <div className="flex gap-2 text-xs">
-                    <span className="text-gray-500 font-semibold">Email</span>
-                    <p className="font-semibold"> {reservation.email}</p>
-                  </div>
-                  <div className="flex gap-2 text-xs">
-                    <span className="text-gray-500 font-semibold">ID</span>
-                    <p className="font-semibold"> {reservation.id}</p>
-                  </div>
-                </div>
-              );
-            })
+                );
+              })
           ) : (
-            <li>No reservations for {date}</li>
+            <div className="bg-white">Brak rezerwacji w tym miesiącu</div>
           )}
         </ul>
       </div>
