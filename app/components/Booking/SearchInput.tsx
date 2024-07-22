8820deffc69dbdb5b9a2ebac664c365d06b1fee1
molszewski34 <m.olszewski341@gmail.com>
2024-07-22 10:33:22 +0200

    feat: added btn to clean query results

--- app/components/Booking/SearchInput.tsx
+++ app/components/Booking/SearchInput.tsx
@@ -10,17 +10,22 @@ const SearchInput: React.FC<SearchInputProps> = ({
   setSearchQuery,
 }) => (
   <label
-    className="flex border border-gray-300 rounded-md py-1 px-2 w-3/4"
+    className="flex border border-gray-300 rounded-md pl-[8.5px] py-[2px] px-2 w-3/4"
     htmlFor="search"
   >
-    <i className="text-lg">search</i>
+    <i className="text-xl text-gray-400">search</i>
     <input
-      className="w-full"
+      className="w-full text-xs"
       type="text"
       id="search"
       value={searchQuery}
       onChange={(e) => setSearchQuery(e.target.value)}
     />
+    {searchQuery.length > 0 && (
+      <button className="w-[28px]">
+        <i onClick={() => setSearchQuery('')}>close</i>
+      </button>
+    )}
   </label>
 );
 
