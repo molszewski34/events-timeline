be618f72590368dd1ebaaf475674fcfc87d62ac9
molszewski34 <m.olszewski341@gmail.com>
2024-07-23 11:05:09 +0200

    style: added borders, widhth, font-weight and padding

--- app/components/Clients/ClientsList.tsx
+++ app/components/Clients/ClientsList.tsx
@@ -1,27 +1,27 @@
 import React from 'react';
 
 const ClientsList = ({ filteredReservations }) => {
-  // const filteredData = data.filter(
-  //     (item) =>
-  //       item.name.toLowerCase().includes(searchQuery.toLowerCase()) ||
-  //       item.phone.includes(searchQuery)
-  //   );
-
   return (
-    <table>
-      <thead>
-        <tr className="border-b ">
-          <th>#</th>
-          <th>Osoba</th>
-          <th>Telefon</th>
-          <th>Email</th>
+    <table className="flex flex-col overflow-x-scroll">
+      <thead className=" ">
+        <tr className="text-sm text-left h-10 w-full border-b ">
+          <th className="font-light text-center border-r min-w-14">#</th>
+          <th className="font-extralight px-5 min-w-[250px]">Osoba</th>
+          <th className="font-extralight px-5 min-w-[250px]">Telefon</th>
+          <th className="font-extralight px-5 min-w-[250px]">Email</th>
         </tr>
       </thead>
-      <tbody>
+      <tbody className="flex flex-col">
         {filteredReservations.map((reservation, index) => (
-          <tr key={reservation.id}>
-            <td>{index}</td>
-            <td>{reservation.main_guest}</td>
+          <tr className="font-light flex" key={reservation.id}>
+            <td className="text-center font-light  border-r min-w-14 h-10">
+              {index}
+            </td>
+            <td className="text-center flex px-5 flex-nowrap min-w-[250px] h-10">
+              {reservation.main_guest}
+            </td>
+            <td className="px-5 min-w-[250px] h-10">{reservation.phone}</td>
+            <td className="px-5 min-w-[250px] h-10">{reservation.email}</td>
           </tr>
         ))}
       </tbody>
