148d2766a574696be1f1c7d2c43faa91fdecdff4
molszewski34 <m.olszewski341@gmail.com>
2024-07-23 08:23:34 +0200

    feat: created clients route and copied some basic css

--- app/clients/page.tsx
+++ app/clients/page.tsx
@@ -1,7 +1,26 @@
-import React from 'react';
+'use client';
+import { useState, useEffect } from 'react';
 
+import SearchInput from '../components/Clients/SearchInput';
 const Clients = () => {
-  return <div>Clients</div>;
+  const [searchQuery, setSearchQuery] = useState('');
+  return (
+    <div className="flex flex-col items-center justify-center bg-white mt-6 mx-2 border border-gray-300 border-b-0 py-1">
+      <div className="flex flex-col px-2 w-full gap-4">
+        <div className="flex justify-between">
+          <header className="flex bg-white w-full">
+            <div className="flex items-center gap-4">
+              <p className="text-sm font-semibold">Klienci</p>
+            </div>
+          </header>
+          <SearchInput
+            searchQuery={searchQuery}
+            setSearchQuery={setSearchQuery}
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Clients;
