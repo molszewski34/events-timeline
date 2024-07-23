cc4cad4e2c2719bc94bd6540e785206b4c229c1e
molszewski34 <m.olszewski341@gmail.com>
2024-07-23 09:55:39 +0200

    feat: rebranded useReservation hook to serve other query purpose

---
+++ app/components/Clients/useClients.ts
@@ -0,0 +1,37 @@
+import { useState, useEffect } from 'react';
+import { Database } from '@/types/supabase';
+import { useQuery } from '@supabase-cache-helpers/postgrest-react-query';
+import { fetchReservations } from '@/app/actions/fetchReservations';
+import useSupabaseBrowser from '@/utils/supabase-browser';
+
+type Reservation = Database['public']['Tables']['reservations']['Row'];
+const useClients = (id: string, searchQuery: string) => {
+    const supabase = useSupabaseBrowser();
+  const { data: reservations, error } = useQuery(fetchReservations(supabase, id));
+
+  const [filteredReservations, setFilteredReservations] = useState([])
+  
+  console.log(filteredReservations)
+
+  useEffect(() => {
+    if (reservations) {
+
+
+   
+        const searched = reservations.filter((reservation) => 
+          reservation.main_guest.toLowerCase().includes(searchQuery.toLowerCase()) ||
+          reservation.phone.toLowerCase().includes(searchQuery.toLowerCase()) ||
+          reservation.email.toLowerCase().includes(searchQuery.toLowerCase()) ||
+          reservation.id.toLowerCase().includes(searchQuery.toLowerCase())
+        );
+        setFilteredReservations(searched);
+      
+
+    }
+  }, [reservations, searchQuery]);
+
+  return { filteredReservations, error };
+
+}
+
+export default useClients
