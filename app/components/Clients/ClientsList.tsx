ccfbf2775cf09bd4159e587bb38e0cf4b916fd83
molszewski34 <m.olszewski341@gmail.com>
2024-07-24 09:23:27 +0200

    feat: implemented sorting feature

--- app/components/Clients/ClientsList.tsx
+++ app/components/Clients/ClientsList.tsx
@@ -1,21 +1,63 @@
-import React from 'react';
+import React, { useMemo } from 'react';
+import { Reservation } from '../RenderRows/types';
+interface ClientsListProps {
+  filteredReservations: Reservation[];
+  isAscending: boolean;
+  setIsAscending: (isAscending: boolean) => void;
+}
 
-const ClientsList = ({ filteredReservations }) => {
+const ClientsList: React.FC<ClientsListProps> = ({
+  filteredReservations,
+  isAscending,
+  setIsAscending,
+}) => {
+  // Sorting function
+  const sortedReservations = useMemo(() => {
+    return filteredReservations.sort((a, b) => {
+      if (a.main_guest < b.main_guest) {
+        return isAscending ? -1 : 1;
+      }
+      if (a.main_guest > b.main_guest) {
+        return isAscending ? 1 : -1;
+      }
+      return 0;
+    });
+  }, [filteredReservations, isAscending]);
+
   return (
     <table className="flex flex-col overflow-x-scroll">
-      <thead className=" ">
-        <tr className="text-sm text-left h-10 w-full border-b ">
+      <thead>
+        <tr className="text-sm text-left h-10 w-full border-b">
           <th className="font-light text-center border-r min-w-14">#</th>
-          <th className="font-extralight px-5 min-w-[250px]">Osoba</th>
+          <th className="flex gap-1 items-center font-extralight px-5 min-w-[250px] h-10">
+            <p>Osoba</p>
+            <span
+              className="flex flex-col text-xl h-10 cursor-pointer"
+              onClick={() => setIsAscending(!isAscending)}
+            >
+              <button
+                className="material-icon h-5 text-gray-300"
+                style={isAscending ? { color: 'black' } : { color: '' }}
+              >
+                arrow_drop_up
+              </button>
+              <button
+                className="material-icon h-5 pt-1 flex items-end text-gray-300"
+                style={!isAscending ? { color: 'black' } : { color: '' }}
+              >
+                arrow_drop_down
+              </button>
+            </span>
+          </th>
           <th className="font-extralight px-5 min-w-[250px]">Telefon</th>
           <th className="font-extralight px-5 min-w-[250px]">Email</th>
         </tr>
       </thead>
       <tbody className="flex flex-col">
-        {filteredReservations.map((reservation, index) => (
+        {sortedReservations.map((reservation, index) => (
           <tr className="font-light flex" key={reservation.id}>
-            <td className="text-center font-light  border-r min-w-14 h-10">
-              {index}
+            <td className="text-center font-light border-r min-w-14 h-10">
+              {index + 1}
             </td>
             <td className="text-center flex px-5 flex-nowrap min-w-[250px] h-10">
               {reservation.main_guest}
