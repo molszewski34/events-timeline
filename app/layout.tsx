2b05f60e366c96d6ac36837c393cd635b46aa853
molszewski34 <m.olszewski341@gmail.com>
2024-07-29 12:09:52 +0200

    feat: added PriceConfigurationWrapper

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,26 @@
 import type { Metadata } from 'next';
 import { Inter as FontSans } from 'next/font/google';
 import './globals.css';
+
 import { CalendarWrapper } from './contexts/Calendar/CalendarProvider';
 import { AddReservationWrapper } from './contexts/AddReservation/AddReservationProvider';
 import { AddRoomWrapper } from './contexts/AddRoom/AddRoomProvider';
 import { SidebarWrapper } from './contexts/Sidebar/SidebarProvider';
-import { cn } from '@/app/lib/utils';
+import { PriceConfigurationWrapper } from './contexts/PriceConfiguration/PriceConfiguration';
+
 import { ReactQueryClientProvider } from './components/ReactQueryClientProvider';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+import Sidebar from './components/Sidebar/Sidebar';
+import NavMobile from './components/Navigation/mobile/NavMobile';
+import AuthButton from './components/Navigation/AuthButton/AuthButton';
+
+import { cn } from '@/app/lib/utils';
+
 const fontSans = FontSans({
   subsets: ['latin'],
   variable: '--font-sans',
 });
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import Sidebar from './components/Sidebar/Sidebar';
-import NavMobile from './components/Navigation/mobile/NavMobile';
-import AuthButton from './components/Navigation/AuthButton/AuthButton';
-import { relative } from 'path';
-// const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -37,18 +41,19 @@ export default function RootLayout({
             fontSans.variable
           )}
         >
-          <CalendarWrapper id="">
-            <SidebarWrapper>
-              <NavMobile>
-                <AuthButton />
-              </NavMobile>
-
-              <Sidebar />
-            </SidebarWrapper>
-            <AddRoomWrapper>
-              <AddReservationWrapper>{children}</AddReservationWrapper>
-            </AddRoomWrapper>
-          </CalendarWrapper>
+          <PriceConfigurationWrapper>
+            <CalendarWrapper>
+              <SidebarWrapper>
+                <NavMobile>
+                  <AuthButton />
+                </NavMobile>
+                <Sidebar />
+                <AddRoomWrapper>
+                  <AddReservationWrapper>{children}</AddReservationWrapper>
+                </AddRoomWrapper>
+              </SidebarWrapper>
+            </CalendarWrapper>
+          </PriceConfigurationWrapper>
         </body>
         <ReactQueryDevtools
           buttonPosition="bottom-left"
