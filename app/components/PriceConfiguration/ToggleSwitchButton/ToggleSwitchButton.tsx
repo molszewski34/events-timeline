4fe26e357c0675b7a52a7a2bfaf676f061b1bf44
molszewski34 <m.olszewski341@gmail.com>
2024-07-30 11:02:04 +0200

    feat: changed text color, added useEffect hook for reusable switches

--- app/components/PriceConfiguration/ToggleSwitchButton/ToggleSwitchButton.tsx
+++ app/components/PriceConfiguration/ToggleSwitchButton/ToggleSwitchButton.tsx
@@ -1,33 +1,42 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
-const ToggleSwitchComponent = ({ question }) => {
+const ToggleSwitchComponent = ({ question, onToggle }) => {
   const [isToggled, setIsToggled] = useState(false);
 
   const handleToggle = () => {
     setIsToggled(!isToggled);
   };
 
+  useEffect(() => {
+    if (isToggled) {
+      onToggle(true);
+    }
+  }, [isToggled, onToggle]);
+
   return (
-    <div className="flex items-center gap-2">
-      <h1 className={isToggled ? 'text-green-500' : ''}>{question}</h1>
-      <label className="relative inline-block w-8 h-5">
-        <input
-          type="checkbox"
-          className="opacity-0 w-0 h-0"
-          checked={isToggled}
-          onChange={handleToggle}
-        />
-        <span
-          className={`absolute cursor-pointer top-0 left-0 right-0 bottom-0 bg-gray-300 rounded-full transition-all ${
-            isToggled ? 'bg-green-500' : ''
-          }`}
-        ></span>
-        <span
-          className={`absolute left-[2px] bottom-[2px] bg-white w-4 h-4 rounded-full transition-transform transform ${
-            isToggled ? 'translate-x-3' : ''
-          }`}
-        ></span>
-      </label>
+    <div className="flex flex-col  gap-2 text-sm text-gray-600">
+      <h1 className={isToggled ? 'text-green-600' : ''}>{question}</h1>
+      <div className="flex gap-2">
+        <label className="relative inline-block w-8 h-5">
+          <input
+            type="checkbox"
+            className="opacity-0 w-0 h-0"
+            checked={isToggled}
+            onChange={handleToggle}
+          />
+          <span
+            className={`absolute cursor-pointer top-0 left-0 right-0 bottom-0 bg-gray-300 rounded-full transition-all ${
+              isToggled ? 'bg-green-600' : ''
+            }`}
+          ></span>
+          <span
+            className={`absolute left-[2px] bottom-[2px] bg-white w-4 h-4 rounded-full transition-transform transform ${
+              isToggled ? 'translate-x-3' : ''
+            }`}
+          ></span>
+        </label>
+        <p className="text-gray-600">TAK</p>
+      </div>
     </div>
   );
 };
