aaeea3f9452fee4a42463d058dc7f28e9eb9505a
molszewski34 <m.olszewski341@gmail.com>
2024-08-01 10:48:14 +0200

    style: added con style to btn and box index 0

--- app/components/PriceConfiguration/Switches/ConfigureChildPrice/ConfigureChildPrice.tsx
+++ app/components/PriceConfiguration/Switches/ConfigureChildPrice/ConfigureChildPrice.tsx
@@ -41,7 +41,10 @@ const ConfigureChildPrice = () => {
   return (
     <div className="flex flex-col border border-gray-200 py-2 px-4 mt-2 w-full">
       {ageRanges.map((range, index) => (
-        <div key={index} className="flex gap-2 w-full mb-2">
+        <div
+          key={index}
+          className={`flex gap-2 w-full mb-2 ${index === 0 ? 'pr-8' : ''}`}
+        >
           <div className="flex flex-col gap-2 w-full">
             <label className="text-sm text-gray-600">Wiek od</label>
             <button
@@ -132,16 +135,22 @@ const ConfigureChildPrice = () => {
           </div>
           {index > 0 && (
             <button
-              className="text-red-600 hover:text-red-800"
+              className="text-red-600 hover:text-red-800 material-icon text-xl"
               onClick={() => handleRemoveAgeRange(index)}
             >
-              Usuń
+              delete
             </button>
           )}
         </div>
       ))}
       <button
-        className="flex gap-2 text-sm justify-center items-center"
+        className={`flex gap-2 text-sm justify-center items-center ${
+          ageRanges.some(
+            (range) => range.minAge === null || range.maxAge === null
+          )
+            ? 'text-gray-300'
+            : 'text-green-500'
+        }`}
         onClick={handleAddAgeRange}
         disabled={ageRanges.some(
           (range) => range.minAge === null || range.maxAge === null
