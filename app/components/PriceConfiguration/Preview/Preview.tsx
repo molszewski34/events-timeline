efea35db50dd4e0eee9e9272c28d0b73b9951130
molszewski34 <m.olszewski341@gmail.com>
2024-08-08 10:40:11 +0200

    feat: added con rendered PriceSections with props title, label, price, subLabel, key

--- app/components/PriceConfiguration/Preview/Preview.tsx
+++ app/components/PriceConfiguration/Preview/Preview.tsx
@@ -2,50 +2,149 @@ import { useState } from 'react';
 import { usePriceConfigurationContext } from '@/app/contexts/PriceConfiguration/PriceConfiguration';
 import Header from './Header';
 import PriceSection from './PriceSection';
+import PriceInput from './PriceInput';
+
+type ChildAge = {
+  minAge: number;
+  maxAge: number;
+};
 
 const Preview = () => {
   const [isOpen, setIsOpen] = useState(false);
-  const { selectedCurrency, partialOccupancyPrice, weekendPrice } =
-    usePriceConfigurationContext();
+  const {
+    selectedCurrency,
+    partialOccupancyPrice,
+    weekendPrice,
+    childPrice,
+    ageRanges,
+    stayDuration,
+    longStay,
+    setLongStay,
+    shortStay,
+    setShortStay,
+  } = usePriceConfigurationContext();
+
+  console.log(ageRanges); // Ensure this has a value
 
   // Example prices with two decimal places
   const standardPrice = 80.0;
   const standardWeekendPrice = 100.0;
   const standardSinglePrice = 60.0;
   const standardSingleWeekendPrice = 80.0;
+  const standardChildPrice = 0.0;
+  const standardWeekendChildPrice = 0.0;
 
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col items-center">
       <Header isOpen={isOpen} setIsOpen={setIsOpen} />
       {isOpen && (
-        <div className="flex flex-col p-2 border-2 border-gray-100 mt-2 cursor-not-allowed w-full max-w-[370px] place-self-center">
+        <div className="flex flex-col p-2 border-2 border-gray-100 mt-2 w-auto max-w-full place-self-center items-center">
           <header>
             <div className="flex gap-2 text-sm font-semibold mb-3">
               <p> Wpisz ceny w walucie:</p>{' '}
               <span className="text-green-600">{selectedCurrency}</span>
             </div>
           </header>
-          {partialOccupancyPrice && (
+          <div className="flex flex-col">
             <PriceSection
-              title="Osoba dorosła (x2)"
+              title={partialOccupancyPrice ? 'Osoba dorosła (x2)' : 'Cena'}
               prices={[
-                { label: 'Standard', price: standardPrice },
-                { label: 'Standard Weekend', price: standardWeekendPrice },
+                { label: 'Standard', price: standardPrice, subLabel: '' },
+                ...(weekendPrice
+                  ? [{ label: 'Standard Weekend', price: standardWeekendPrice }]
+                  : []),
+                ...(stayDuration
+                  ? [
+                      {
+                        label: 'Długi pobyt',
+                        price: standardWeekendPrice,
+                        subLabel: `od ${longStay}`,
+                      },
+                    ]
+                  : []),
+                ...(stayDuration
+                  ? [
+                      {
+                        label: 'Krótki pobyt',
+                        price: standardWeekendPrice,
+                        subLabel: `do ${shortStay}`,
+                      },
+                    ]
+                  : []),
               ]}
             />
-          )}
-          {weekendPrice && (
-            <PriceSection
-              title="Osoba dorosła (x1)"
-              prices={[
-                { label: 'Standard', price: standardSinglePrice },
-                {
-                  label: 'Standard Weekend',
-                  price: standardSingleWeekendPrice,
-                },
-              ]}
-            />
-          )}
+            {partialOccupancyPrice && (
+              <PriceSection
+                title={partialOccupancyPrice ? 'Osoba dorosła (x1)' : 'Cena'}
+                prices={[
+                  { label: 'Standard', price: standardPrice },
+                  ...(weekendPrice
+                    ? [
+                        {
+                          label: 'Standard Weekend',
+                          price: standardWeekendPrice,
+                        },
+                      ]
+                    : []),
+                  ...(stayDuration
+                    ? [
+                        {
+                          label: 'Długi pobyt ',
+                          price: standardWeekendPrice,
+                          subLabel: `od ${longStay}`,
+                        },
+                      ]
+                    : []),
+                  ...(stayDuration
+                    ? [
+                        {
+                          label: 'Krótki pobyt',
+                          price: standardWeekendPrice,
+                          subLabel: `do ${shortStay}`,
+                        },
+                      ]
+                    : []),
+                ]}
+              />
+            )}
+          </div>
+          <>
+            {ageRanges.map((childAge: ChildAge) => (
+              <PriceSection
+                key={`${childAge.minAge}-${childAge.maxAge}`}
+                title={`Dziecko (${childAge.minAge}-${childAge.maxAge})`}
+                prices={[
+                  { label: 'Standard', price: standardPrice, subLabel: '' },
+                  ...(weekendPrice
+                    ? [
+                        {
+                          label: 'Standard Weekend',
+                          price: standardWeekendPrice,
+                        },
+                      ]
+                    : []),
+                  ...(stayDuration
+                    ? [
+                        {
+                          label: 'Długi pobyt',
+                          price: standardWeekendPrice,
+                          subLabel: `od ${longStay}`,
+                        },
+                      ]
+                    : []),
+                  ...(stayDuration
+                    ? [
+                        {
+                          label: 'Krótki pobyt',
+                          price: standardWeekendPrice,
+                          subLabel: `do ${shortStay}`,
+                        },
+                      ]
+                    : []),
+                ]}
+              />
+            ))}
+          </>
         </div>
       )}
     </div>
