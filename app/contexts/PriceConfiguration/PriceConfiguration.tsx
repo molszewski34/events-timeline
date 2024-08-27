9eee5ab9bb7b783c1b771f987029ee20fffe4250
molszewski34 <m.olszewski341@gmail.com>
2024-08-27 08:16:19 +0200

    refactor: removed unused states

--- app/contexts/PriceConfiguration/PriceConfiguration.tsx
+++ app/contexts/PriceConfiguration/PriceConfiguration.tsx
@@ -11,66 +11,11 @@ export function PriceConfigurationWrapper({
 }: {
   children: React.ReactNode;
 }) {
-  const [isReminding, setIsReminding] = useState(true);
   const [priceSettings, setPriceSettings] = useState(initialPriceConfiguration);
-  const [partialOccupancyPrice, setPartialOccupancyPrice] = useState(false);
-  const [weekendPrice, setWeekendPrice] = useState(false);
-  const [stayDuration, setStayDuration] = useState(false);
-  const [childPrice, setChildPrice] = useState(false);
-  const [mealPrice, setMealPrice] = useState(false);
-  const [localTaxAmount, setLocalTaxAmount] = useState(false);
-  const [bookingRestrictions, setBookingRestrictions] = useState(false);
-  const [selectedCurrency, setSelectedCurrency] = useState('EUR');
-  const [selectedMinAge, setSelectedMinAge] = useState(null);
-  const [selectedMaxAge, setSelectedMaxAge] = useState(null);
-  const [addCostToAdult, setAddCostToAdult] = useState(false);
-  const [localTaxForChild, setLocalTaxForChild] = useState('');
-  const [minAgeListOpen, setMinAgeListOpen] = useState<number | null>(null);
-  const [maxAgeListOpen, setMaxAgeListOpen] = useState<number | null>(null);
-  const [ageRanges, setAgeRanges] = useState<
-    { minAge: number | null; maxAge: number | null }[]
-  >([{ minAge: null, maxAge: null }]);
-  const [longStay, setLongStay] = useState<number>(0);
-  const [shortStay, setShortStay] = useState<number>(0);
+
   return (
     <PriceConfiguration.Provider
       value={{
-        isReminding,
-        setIsReminding,
-        partialOccupancyPrice,
-        setPartialOccupancyPrice,
-        weekendPrice,
-        setWeekendPrice,
-        stayDuration,
-        setStayDuration,
-        childPrice,
-        setChildPrice,
-        mealPrice,
-        setMealPrice,
-        localTaxAmount,
-        setLocalTaxAmount,
-        bookingRestrictions,
-        setBookingRestrictions,
-        selectedCurrency,
-        setSelectedCurrency,
-        selectedMinAge,
-        setSelectedMinAge,
-        selectedMaxAge,
-        setSelectedMaxAge,
-        addCostToAdult,
-        setAddCostToAdult,
-        localTaxForChild,
-        setLocalTaxForChild,
-        minAgeListOpen,
-        setMinAgeListOpen,
-        maxAgeListOpen,
-        setMaxAgeListOpen,
-        ageRanges,
-        setAgeRanges,
-        longStay,
-        setLongStay,
-        shortStay,
-        setShortStay,
         priceSettings,
         setPriceSettings,
       }}
