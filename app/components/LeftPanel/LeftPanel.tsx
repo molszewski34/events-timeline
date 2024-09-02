44f7db4db989c4a2725201975a1f0b3c1f953653
molszewski34 <m.olszewski341@gmail.com>
2024-09-02 10:01:33 +0200

    style: changed height of main container added items-center and flex

--- app/components/LeftPanel/LeftPanel.tsx
+++ app/components/LeftPanel/LeftPanel.tsx
@@ -48,8 +48,8 @@ const LeftPanel = ({ id }: { id: string }) => {
     setRoomFormData(newRoomFormData);
   };
   return (
-    <div className="fixed   bg-white z-[50] min-w-[100px] flex flex-col">
-      <div className="h-[50px] text-xs text-left p-2 border-2 border-l-0 text-gray-500">
+    <div className="fixed  bg-white z-[50] w-[100px] flex flex-col">
+      <div className="h-[42px] text-xs text-left p-2 border-2 border-l-0 text-gray-500 items-center flex">
         <i className="material-icons text-2xl">filter_alt</i>
       </div>
       {rooms?.map((room: Room) => (
