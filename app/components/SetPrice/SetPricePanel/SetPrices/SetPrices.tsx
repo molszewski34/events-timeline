59d261821b4626267117fa0f355f959d820ee3fe
molszewski34 <m.olszewski341@gmail.com>
2024-09-12 08:22:11 +0200

    initial commit

--- app/components/SetPrice/SetPricePanel/SetPrices/SetPrices.tsx
+++ app/components/SetPrice/SetPricePanel/SetPrices/SetPrices.tsx
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useQuery } from '@supabase-cache-helpers/postgrest-react-query';
+import { fetchRooms } from '@/app/actions/fetchRoom';
+import useSupabaseBrowser from '@/utils/supabase-browser';
+import SetPartialPrice from './SetPartialPrice/SetPartialPrice';
 
-const SetPrices = () => {
-  return <div>SetPrices</div>;
+const SetPrices = ({ id }: { id: string }) => {
+  const supabase = useSupabaseBrowser();
+
+  const { data: rooms } = useQuery(fetchRooms(supabase, id));
+  return <SetPartialPrice id="id" />;
 };
 
 export default SetPrices;
