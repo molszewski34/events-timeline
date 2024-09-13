a3091b99410b86022e61e8d1e98dda56e82af275
molszewski34 <m.olszewski341@gmail.com>
2024-09-13 08:43:11 +0200

    feat: added prop readOnly

--- app/components/PriceConfiguration/Preview/PriceInput.tsx
+++ app/components/PriceConfiguration/Preview/PriceInput.tsx
@@ -4,9 +4,15 @@ interface PriceInputProps {
   label: string;
   price: number;
   subLabel?: number | string;
+  readOnly?: boolean; // New prop to control readOnly state
 }
 
-const PriceInput: React.FC<PriceInputProps> = ({ label, price, subLabel }) => (
+const PriceInput: React.FC<PriceInputProps> = ({
+  label,
+  price,
+  subLabel,
+  readOnly = true,
+}) => (
   <div className="flex flex-col gap-2">
     <div className="flex flex-col">
       <label className="text-xs text-gray-500 font-medium" htmlFor={label}>
@@ -17,7 +23,7 @@ const PriceInput: React.FC<PriceInputProps> = ({ label, price, subLabel }) => (
       )}
       <input
         className="text-xs border border-gray-300 py-2 px-1 text-right max-w-36"
-        readOnly
+        readOnly={readOnly}
         type="text"
         defaultValue={price.toFixed(2)}
       />
