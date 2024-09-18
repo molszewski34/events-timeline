884cd210019851e5a6bb50b6e29a3983da81cda8
molszewski34 <m.olszewski341@gmail.com>
2024-09-18 08:18:26 +0200

    feat: added inputs defined by priceConfiguration and connected them to db

--- app/components/SetPrice/SetPricePanel/SetPrices/SetPartialPrice/SetPartialPrice.tsx
+++ app/components/SetPrice/SetPricePanel/SetPrices/SetPartialPrice/SetPartialPrice.tsx
@@ -4,19 +4,23 @@ import { fetchRooms } from '@/app/actions/fetchRoom';
 import useSupabaseBrowser from '@/utils/supabase-browser';
 import PriceSection from '@/app/components/PriceConfiguration/Preview/PriceSection';
 import { useSetPriceContext } from '@/app/contexts/SetPrice/SetPriceProvider';
+import { usePriceConfigurationContext } from '@/app/contexts/PriceConfiguration/PriceConfiguration';
+import { fetchPriceSettings } from '@/app/actions/fetchPriceSettings';
 
 const SetPartialPrice = ({ id }: { id: string }) => {
   const supabase = useSupabaseBrowser();
 
   const { data: rooms } = useQuery(fetchRooms(supabase, id));
+  const { data: priceConfiguration, isLoading: isLoadingPriceConfig } =
+    useQuery(fetchPriceSettings(supabase));
+
   const { priceFormData, setPriceFormData } = useSetPriceContext();
 
   const [maxNumOfPersons, setMaxNumOfPersons] = useState(0);
-
   const [priceSections, setPriceSections] = useState<JSX.Element[]>([]);
 
   useEffect(() => {
-    if (priceFormData.selectedRooms.length > 0) {
+    if (priceFormData?.selectedRooms?.length > 0) {
       const maxPersons = priceFormData.selectedRooms.reduce(
         (max, room) => Math.max(max, room.num_of_persons || 0),
         0
@@ -26,22 +30,108 @@ const SetPartialPrice = ({ id }: { id: string }) => {
   }, [priceFormData.selectedRooms]);
 
   useEffect(() => {
-    if (maxNumOfPersons > 0) {
+    if (maxNumOfPersons > 0 && priceConfiguration) {
       const sections = Array.from({ length: maxNumOfPersons }, (_, index) => (
         <PriceSection
           key={index}
           title={`Osoba dorosła (x${index + 1})`}
-          prices={[]}
+          readOnly={false}
+          prices={[
+            { label: 'Standard', price: 0, subLabel: '' },
+            ...(priceConfiguration?.weekend_price
+              ? [
+                  {
+                    label: 'Standard Weekend',
+                    price: 0,
+                  },
+                ]
+              : []),
+            ...(priceConfiguration?.stay_duration
+              ? [
+                  {
+                    label: 'Długi pobyt',
+                    price: 0,
+                    subLabel: `od ${priceConfiguration?.long_stay}`,
+                  },
+                ]
+              : []),
+            ...(priceConfiguration?.stay_duration
+              ? [
+                  {
+                    label: 'Krótki pobyt',
+                    price: 0,
+                    subLabel: `do ${priceConfiguration?.short_stay}`,
+                  },
+                ]
+              : []),
+          ]}
         />
       ));
 
       const sortedSections = sections.reverse();
-
       setPriceSections(sortedSections);
     }
-  }, [maxNumOfPersons]);
+  }, [maxNumOfPersons, priceConfiguration]);
 
-  return <>{priceSections}</>;
+  if (isLoadingPriceConfig) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div className="flex flex-col gap-2">
+      <div className="">{priceSections}</div>
+      <div className="">
+        {priceConfiguration.child_price && (
+          <>
+            {priceConfiguration.age_ranges.map(
+              (childAge: ChildAge) =>
+                childAge.minAge !== null &&
+                childAge.maxAge !== null && (
+                  <PriceSection
+                    key={`${childAge.minAge}-${childAge.maxAge}`}
+                    title={`Dziecko (${childAge.minAge}-${childAge.maxAge})`}
+                    readOnly={false}
+                    prices={[
+                      {
+                        label: 'Standard',
+                        price: 0,
+                        subLabel: '',
+                      },
+                      ...(priceConfiguration.weekend_price
+                        ? [
+                            {
+                              label: 'Standard Weekend',
+                              price: 0,
+                            },
+                          ]
+                        : []),
+                      ...(priceConfiguration.stay_duration
+                        ? [
+                            {
+                              label: 'Długi pobyt',
+                              price: 0,
+                              subLabel: `(od ${priceConfiguration.long_stay})`,
+                            },
+                          ]
+                        : []),
+                      ...(priceConfiguration.stay_duration
+                        ? [
+                            {
+                              label: 'Krótki pobyt',
+                              price: 0,
+                              subLabel: `(do ${priceConfiguration.short_stay})`,
+                            },
+                          ]
+                        : []),
+                    ]}
+                  />
+                )
+            )}
+          </>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default SetPartialPrice;
