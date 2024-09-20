1cd7fc5605fbbe707ef3fb3a19e18488c1698d2e
molszewski34 <m.olszewski341@gmail.com>
2024-09-20 08:08:30 +0200

    feat: added RecalculatePrices and Footer component

--- app/components/SetPrice/SetPricePanel/SetPricePanel.tsx
+++ app/components/SetPrice/SetPricePanel/SetPricePanel.tsx
@@ -7,6 +7,8 @@ import SetPriceDatePickerSection from './SetPriceDatePickerSection/SetPriceDateP
 import RoomSelector from '../SetRoom/SetRoom';
 import NextRoom from '../SetRoom/NextRoom/NextRoom';
 import SetPrices from './SetPrices/SetPrices';
+import Footer from './Footer/Footer';
+import RecalculatePrices from './RecalculatePrices/RecalculatePrices';
 
 const SetPricePanel = () => {
   const { openSetPricePanel, setOpenSetPricePanel } = useSetPriceContext();
@@ -16,11 +18,13 @@ const SetPricePanel = () => {
       style={{ display: openSetPricePanel ? 'flex' : 'none' }}
       className="fixed w-[92vw] max-w-[80em]  justify-self-center top-[2vh] bottom-[2vh] right-[4vw] left-[4vw]  bg-white z-[999] p-[1.27em] flex flex-col gap-4 overflow-y-scroll"
     >
-      <div className="relative flex flex-col gap-2  ">
+      <div className="relative flex flex-col gap-4  ">
         <SetPriceHeader />
         <SetPriceDatePickerSection />
         <RoomSelector id="id" />
-        <SetPrices />
+        <SetPrices id="id" />
+        <RecalculatePrices />
+        <Footer />
       </div>
     </main>
   );
