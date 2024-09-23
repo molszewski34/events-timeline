c60683a1596b734c52ebf9de16ee939974febdf7
molszewski34 <m.olszewski341@gmail.com>
2024-09-23 10:27:47 +0200

    feat: added onInputChange as props to identify input by field

--- app/components/PriceConfiguration/Preview/PriceSection.tsx
+++ app/components/PriceConfiguration/Preview/PriceSection.tsx
@@ -5,29 +5,33 @@ interface Price {
   label: string;
   subLabel?: number | string;
   price: number;
+  field?: string; // Dodano pole do identyfikacji
 }
 
 interface PriceSectionProps {
   title: string;
   prices: Price[];
   readOnly: boolean;
+  onInputChange: (field: string, value: string) => void;
 }
 
 const PriceSection: React.FC<PriceSectionProps> = ({
   title,
   prices,
   readOnly = true,
+  onInputChange,
 }) => (
   <div className="flex flex-col border-b border-gray-300 pb-4">
     <header className="text-sm text-gray-500">{title}</header>
     <div className="flex justify-between flex-wrap mt-2 gap-2">
-      {prices.map(({ label, price, subLabel }) => (
+      {prices.map(({ label, price, subLabel, field }) => (
         <PriceInput
           key={label}
           label={label}
           price={price}
           subLabel={subLabel}
           readOnly={readOnly}
+          onChange={(value) => onInputChange(field, value)} // Przekazuje onChange
         />
       ))}
     </div>
