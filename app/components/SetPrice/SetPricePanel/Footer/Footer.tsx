6f24f1f47949bff52ebc69062293e6bdfc120daf
molszewski34 <m.olszewski341@gmail.com>
2024-10-04 08:14:39 +0200

    feat: added new func to close panel, added SetPriceSubmitBtn component

--- app/components/SetPrice/SetPricePanel/Footer/Footer.tsx
+++ app/components/SetPrice/SetPricePanel/Footer/Footer.tsx
@@ -1,14 +1,24 @@
 import React from 'react';
 
 import SubmitButton from '@/app/components/utils/SubmitButton';
-
+import { useSetPriceContext } from '@/app/contexts/SetPrice/SetPriceProvider';
+import { useCalendarContext } from '@/app/contexts/Calendar/CalendarProvider';
+import SetPriceSubmitBtn from '../SetPriceSubmitBtn/SetPriceSubmitBtn';
 const Footer: React.FC = () => {
+  const { openSetPricePanel, setOpenSetPricePanel } = useSetPriceContext();
+  const { setOverlay } = useCalendarContext();
   return (
     <div className="flex items-center justify-around">
-      <button className="flex gap-1 bg-gray-300 text-sm font-semibold  w-full items-center justify-center  rounded-sm h-8">
+      <button
+        onClick={() => {
+          setOpenSetPricePanel(!openSetPricePanel);
+          setOverlay(false);
+        }}
+        className="flex gap-1 bg-gray-300 text-sm font-semibold  w-full items-center justify-center  rounded-sm h-8"
+      >
         Zamknij
       </button>
-      <SubmitButton text="Zapisz" icon="save" />
+      <SetPriceSubmitBtn />
     </div>
   );
 };
