8042358845c0ee33d17aad87d5c79f203f77bfc2
molszewski34 <m.olszewski341@gmail.com>
2024-10-09 09:07:56 +0200

    refactor: separated const rows to new file

--- app/components/RenderRows/RenderRows.tsx
+++ app/components/RenderRows/RenderRows.tsx
@@ -1,42 +1,35 @@
 'use client';
-import React, {
-  useState,
-  useEffect,
-  useCallback,
-  useMemo,
-  useRef,
-} from 'react';
-import {
-  addDays,
-  differenceInDays,
-  format,
-  isSameDay,
-  isToday,
-  isWeekend,
-  startOfWeek,
-  endOfWeek,
-} from 'date-fns';
+
+import React, { useState, useEffect, useCallback, useMemo } from 'react';
+import { addDays, differenceInDays } from 'date-fns';
 import { useCalendarContext } from '@/app/contexts/Calendar/CalendarProvider';
 import { useAddReservationContext } from '@/app/contexts/AddReservation/AddReservationProvider';
 import { useAddRoomContext } from '@/app/contexts/AddRoom/AddRoomProvider';
 import { fetchRooms } from '@/app/actions/fetchRoom';
 import { fetchReservations } from '@/app/actions/fetchReservations';
-import { FormData } from '@/app/contexts/AddReservation/types';
 import { useSwipeable } from 'react-swipeable';
 import LeftPanel from '../LeftPanel/LeftPanel';
-import AddReservationBtn from '../Reservations/AddReservation/Button/AddReservationBtn';
 import useSupabaseBrowser from '@/utils/supabase-browser';
 import { useQuery } from '@supabase-cache-helpers/postgrest-react-query';
-import { Database } from '@/types/supabase';
-import { pl } from 'date-fns/locale';
 import { usePathname } from 'next/navigation';
 import SetPriceBtn from '../SetPrice/SetPriceBtn/SetPriceBtn';
-import { useSetPriceContext } from '@/app/contexts/SetPrice/SetPriceProvider';
+import AddReservationBtn from '../Reservations/AddReservation/Button/AddReservationBtn';
+
 import Day from './Days/Days';
+import { useSetPriceContext } from '@/app/contexts/SetPrice/SetPriceProvider';
+import { FormData } from '@/app/contexts/AddReservation/types';
+import { Room } from './types';
+import Reservations from './Rows/Reservations';
+import { fetchPrices } from '@/app/actions/fetchPrices';
+import Prices from './Rows/Prices';
 export default function RenderRows({ id }: { id: string }) {
   const supabase = useSupabaseBrowser();
   const { data: reservations } = useQuery(fetchReservations(supabase, id));
   const { data: rooms } = useQuery(fetchRooms(supabase, id));
+  const { data: prices } = useQuery(fetchPrices(supabase, id));
+
+  // console.log(prices);
+
   const [isButtonVisible, setIsButtonVisible] = useState(false);
 
   const pathname = usePathname();
@@ -53,104 +46,26 @@ export default function RenderRows({ id }: { id: string }) {
     setEndDate,
     startDate,
     setStartDate,
-    isEditing,
-    setIsEditing,
-    setOverlay,
   } = useCalendarContext();
 
-  const {
-    formData,
-    setFormData,
-    selectedButton,
-    setSelectedButton,
-    setOpenAddReservationPanel,
-  } = useAddReservationContext();
-
+  const { setFormData, setSelectedButton } = useAddReservationContext();
   const { setPriceFormData } = useSetPriceContext();
-
   const { setFetchedRooms } = useAddRoomContext();
-  const originalFormDataRef = useRef<FormData | null>(null);
 
   const [hoveredColumnIndex, setHoveredColumnIndex] = useState<number | null>(
     null
   );
   const [hoveredRowIndex, setHoveredRowIndex] = useState<number | null>(null);
 
-  useEffect(() => {
-    const today = new Date();
-    const monday = startOfWeek(today, { weekStartsOn: 1 }); //
-    setStartDate(monday);
-    setEndDate(endOfWeek(monday, { weekStartsOn: 1 })); //
-  }, [setStartDate, setEndDate]);
-
-  const dateFormat = 'EEEEEE dd';
-
-  const handleButtonClick = useCallback(
-    (room, timestamp) => {
-      setSelectedButton({ room, timestamp });
-    },
-    [setSelectedButton]
-  );
-
-  const handlePrevWeek = useCallback(() => {
-    setStartDate((prev) => addDays(prev, -7));
-    setEndDate((prev) => addDays(prev, -7));
-  }, [setStartDate, setEndDate]);
-
-  const handleNextWeek = useCallback(() => {
-    setStartDate((prev) => addDays(prev, 7));
-    setEndDate((prev) => addDays(prev, 7));
-  }, [setStartDate, setEndDate]);
-
-  const handlers = useSwipeable({
-    onSwipedLeft: handleNextWeek,
-    onSwipedRight: handlePrevWeek,
-    trackMouse: true,
-  });
-
-  const handleSetFormData = useCallback(
-    (reservation) => {
-      setFormData((prevData) => ({
-        ...prevData,
-        ...reservation,
-      }));
-    },
-    [setFormData]
-  );
-
-  useEffect(() => {
-    if (isEditing && selectedButton?.room) {
-      if (!originalFormDataRef.current) {
-        originalFormDataRef.current = formData;
-      }
-
-      const reservation = reservations?.find(
-        (res) =>
-          res.room_id === selectedButton.room.id &&
-          isSameDay(
-            new Date(res.selected_start_date),
-            new Date(selectedButton.timestamp)
-          )
-      );
-
-      if (reservation) {
-        handleSetFormData(reservation);
-      }
-    } else if (!isEditing && originalFormDataRef.current) {
-      setFormData(originalFormDataRef.current);
-      originalFormDataRef.current = null;
-    }
-  }, [isEditing, selectedButton, reservations, formData, handleSetFormData]);
-
   const handleMouseEnter = useCallback(
-    (room, timestamp) => {
+    (room: Room, timestamp: number) => {
       setSelectedButton({ room, timestamp });
       setIsButtonVisible(true);
-      setFormData((prevData) => ({
+      setFormData((prevData: FormData) => ({
         ...prevData,
         currentDateTimestamp: timestamp,
       }));
-      setPriceFormData((prevData) => ({
+      setPriceFormData((prevData: FormData) => ({
         ...prevData,
         currentDateTimestamp: timestamp,
         room: room,
@@ -158,6 +73,25 @@ export default function RenderRows({ id }: { id: string }) {
     },
     [setSelectedButton]
   );
+
+  const handleButtonClick = useCallback(
+    (room: Room, timestamp: number) => {
+      setSelectedButton({ room, timestamp });
+    },
+    [setSelectedButton]
+  );
+
+  const handleMouseLeave = useCallback(() => {
+    setIsButtonVisible(false);
+    setHoveredColumnIndex(null);
+    setHoveredRowIndex(null);
+  }, []);
+
+  const handlers = useSwipeable({
+    onSwipedLeft: () => setStartDate((prev: Date) => addDays(prev, 7)),
+    onSwipedRight: () => setStartDate((prev: Date) => addDays(prev, -7)),
+    trackMouse: true,
+  });
 
   const days = useMemo(() => {
     const totalDays = differenceInDays(endDate, startDate) + 1;
@@ -170,110 +104,6 @@ export default function RenderRows({ id }: { id: string }) {
       />
     ));
   }, [startDate, endDate, currentDate]);
-
-  const handleMouseLeave = useCallback(() => {
-    setIsButtonVisible(false);
-    setHoveredColumnIndex(null);
-    setHoveredRowIndex(null);
-  }, []);
-
-  const rows = useMemo(() => {
-    return rooms?.map((room, roomIndex) => {
-      const totalDays = differenceInDays(endDate, startDate) + 1;
-      const roomReservations = reservations?.filter(
-        (res) => res.room_id === room.id
-      );
-
-      const roomDays = Array.from({ length: totalDays }, (_, index) => {
-        const currentDateIterator = addDays(startDate, index);
-        const isWeekendDay = isWeekend(currentDateIterator);
-        const isCurrentDay = isToday(currentDateIterator);
-        const currentDateTimestamp = currentDateIterator.getTime();
-
-        let reservation = roomReservations?.find((res) =>
-          isSameDay(new Date(res.selected_start_date), currentDateIterator)
-        );
-        const duration = reservation
-          ? differenceInDays(
-              new Date(reservation.selected_end_date),
-              new Date(reservation.selected_start_date)
-            ) + 1
-          : 1;
-
-        return (
-          <span
-            key={`${room.id}-${currentDateIterator.toString()}`}
-            className={`flex flex-col flex-wrap relative w-[44px] h-[48px] 
-              ${index === 0 ? 'border-l border-t' : 'border-t border-l'} 
-              border-gray-200 ${isWeekendDay ? 'bg-[#ebedef]' : ''} ${
-              isCurrentDay ? 'bg-[#d9f2e3]' : ''
-            }`}
-            onMouseEnter={() => {
-              handleMouseEnter(room, currentDateTimestamp);
-              setHoveredColumnIndex(index);
-              setHoveredRowIndex(roomIndex);
-            }}
-            onTouchStart={() => handleButtonClick(room, currentDateTimestamp)}
-            onMouseLeave={handleMouseLeave}
-          >
-            {isButtonVisible &&
-              selectedButton?.room?.id === room.id &&
-              selectedButton.timestamp === currentDateTimestamp && (
-                <MemoizedButton />
-              )}
-
-            {reservation && (
-              <button
-                className="absolute flex justify-center items-center top-0 bottom-0 left-0 right-0 bg-red-300 text-gray-700 text-sm font-semibold z-[40] border border-slate-50"
-                style={{
-                  width: `${duration * 50}px`,
-                  backgroundColor: reservation.selected_status?.color,
-                }}
-                onClick={() => {
-                  setIsEditing?.(true);
-                  setOpenAddReservationPanel(true);
-                  setOverlay(true);
-                }}
-              >
-                {duration < 3
-                  ? reservation.main_guest
-                      .match(/(\b\S)?/g)
-                      .join('')
-                      .toUpperCase()
-                  : reservation.main_guest || 'Brak Nazwy'}
-              </button>
-            )}
-          </span>
-        );
-      });
-
-      return (
-        <div key={room.id} className="flex relative">
-          {hoveredColumnIndex !== null && hoveredRowIndex === roomIndex && (
-            <div
-              className="absolute inset-0 bg-black opacity-10 pointer-events-none"
-              style={{
-                zIndex: 10,
-              }}
-            />
-          )}
-          {roomDays}
-        </div>
-      );
-    });
-  }, [
-    rooms,
-    reservations,
-    startDate,
-    endDate,
-    selectedButton,
-    handleButtonClick,
-    hoveredColumnIndex,
-    hoveredRowIndex,
-    setIsEditing,
-    setOpenAddReservationPanel,
-    setOverlay,
-  ]);
 
   useEffect(() => {
     if (rooms) {
@@ -313,7 +143,42 @@ export default function RenderRows({ id }: { id: string }) {
             }}
           />
         )}
-        <div className="bg-white ">{rows}</div>
+        {pathname === '/calendar' && (
+          <Reservations
+            rooms={rooms}
+            reservations={reservations}
+            startDate={startDate}
+            endDate={endDate}
+            currentDate={currentDate}
+            isButtonVisible={isButtonVisible}
+            handleButtonClick={handleButtonClick}
+            handleMouseEnter={handleMouseEnter}
+            handleMouseLeave={handleMouseLeave}
+            hoveredColumnIndex={hoveredColumnIndex}
+            hoveredRowIndex={hoveredRowIndex}
+            setHoveredColumnIndex={setHoveredColumnIndex}
+            setHoveredRowIndex={setHoveredRowIndex}
+            MemoizedButton={MemoizedButton}
+          />
+        )}
+        {pathname === '/price/set' && (
+          <Prices
+            rooms={rooms}
+            prices={prices}
+            startDate={startDate}
+            endDate={endDate}
+            currentDate={currentDate}
+            isButtonVisible={isButtonVisible}
+            handleButtonClick={handleButtonClick}
+            handleMouseEnter={handleMouseEnter}
+            handleMouseLeave={handleMouseLeave}
+            hoveredColumnIndex={hoveredColumnIndex}
+            hoveredRowIndex={hoveredRowIndex}
+            setHoveredColumnIndex={setHoveredColumnIndex}
+            setHoveredRowIndex={setHoveredRowIndex}
+            MemoizedButton={MemoizedButton}
+          />
+        )}
       </div>
     </div>
   );
