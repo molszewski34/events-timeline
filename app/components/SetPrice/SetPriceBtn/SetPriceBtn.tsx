d2c80f894cf5ce67590e59cfd01f8c68f555c46f
molszewski34 <m.olszewski341@gmail.com>
2024-10-14 10:30:19 +0200

    style: btn is now opacity-0 in mobile view

--- app/components/SetPrice/SetPriceBtn/SetPriceBtn.tsx
+++ app/components/SetPrice/SetPriceBtn/SetPriceBtn.tsx
@@ -11,7 +11,7 @@ const SetPriceBtn = () => {
         setOpenSetPricePanel(true);
         setOverlay(true);
       }}
-      className="absolute flex justify-center items-center top-0 bottom-0 left-0 right-0 w-[42px] h-[46px]  bg-white  z-[40] shadow-sm cursor-pointer"
+      className="absolute flex justify-center items-center top-0 bottom-0 left-0 right-0 w-[42px] h-[46px]  bg-white  z-[40] shadow-sm cursor-pointer opacity-0 md:opacity-100"
     >
       +
     </div>
