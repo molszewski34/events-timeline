92815fc672c8c57f414f4aef7794739e2bb328a4
molszewski34 <m.olszewski341@gmail.com>
2024-10-14 10:28:36 +0200

    fix: added setPriceFormData state from context and used it to setPriceFormData room

--- app/components/RenderRows/Rows/hooks/useHandleButtonClick.ts
+++ app/components/RenderRows/Rows/hooks/useHandleButtonClick.ts
@@ -1,19 +1,23 @@
 import { useCallback, useState } from "react";
 import { useAddReservationContext } from "@/app/contexts/AddReservation/AddReservationProvider";
+import { useSetPriceContext } from "@/app/contexts/SetPrice/SetPriceProvider";
 interface Room {
     id: number;
     name: string;
   }
 
 function useHandleButtonClick() {
-    const {
-       
-        setSelectedButton,
-      } = useAddReservationContext();
+    const {setSelectedButton} = useAddReservationContext();
+      const {setPriceFormData} = useSetPriceContext()
 
     const handleButtonClick = useCallback(
         (room: Room, timestamp: number) => {
           setSelectedButton({ room, timestamp });
+          setPriceFormData((prevData: FormData) => ({
+            ...prevData,
+            room: room, timestamp
+          }));
+        
         },
         [setSelectedButton]
       );
