3ccbab36e0c73b225a2183f8d901afb427b59d83
molszewski34 <m.olszewski341@gmail.com>
2024-10-16 10:18:47 +0200

    refactor: separated into component from SetRoom and added types

---
+++ app/components/SetPrice/SetRoom/Modal.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import useHandleCheckboxChange from './hooks/useHandleCheckboxChange';
+import useHandleRemoveRoom from './hooks/useHandleRemoveRoom';
+import { useSetPriceContext } from '@/app/contexts/SetPrice/SetPriceProvider';
+import NextRoom from './NextRoom/NextRoom';
+
+interface ModalProps {
+  rooms: [];
+  isNextRoomVisible: boolean;
+  room: string;
+  id: string;
+  name: string;
+}
+
+const Modal: React.FC<ModalProps> = ({ rooms, isNextRoomVisible }) => {
+  const { priceFormData } = useSetPriceContext();
+
+  const { handleCheckboxChange } = useHandleCheckboxChange(rooms);
+  const { handleRemoveRoom } = useHandleRemoveRoom();
+  return (
+    <div className="flex flex-col mb-4 gap-3 relative">
+      <label className="flex items-center gap-2 text-xs">
+        <input
+          type="checkbox"
+          onChange={handleCheckboxChange}
+          disabled={
+            priceFormData.selectedRooms.length === 0 || isNextRoomVisible
+          }
+          className="accent-green-600"
+        />
+        Zaznacz pokoje o tej samej pojemności
+      </label>
+      {isNextRoomVisible && <NextRoom id="id" />}
+
+      <div className="flex gap-1 ml-3">
+        {priceFormData.selectedRooms.map((room: ModalProps) => (
+          <button
+            key={room.id}
+            className="flex gap-2 items-center justify-between text-xs text-white bg-[#00a541] py-1 px-2 rounded h-[23px]"
+            onClick={() => handleRemoveRoom(room.id)}
+          >
+            {room.name}
+            <i className="text-sm">cancel</i>
+          </button>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
