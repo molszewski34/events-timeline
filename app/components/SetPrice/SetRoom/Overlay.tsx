a8cbda3a04b9ba235b0b47d025563d44c652f460
molszewski34 <m.olszewski341@gmail.com>
2024-10-16 10:16:42 +0200

    refactor: created component and added types

---
+++ app/components/SetPrice/SetRoom/Overlay.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+interface OverlayProps {
+  isNextRoomVisible: boolean;
+  setIsNextRoomVisible: (visible: boolean) => void;
+}
+
+const Overlay: React.FC<OverlayProps> = ({
+  isNextRoomVisible,
+  setIsNextRoomVisible,
+}) => {
+  const handleOverlayClick = () => {
+    setIsNextRoomVisible(false);
+  };
+  return (
+    <div>
+      {isNextRoomVisible && (
+        <div
+          className="fixed top-0 bottom-0 left-0 right-0  bg-black bg-opacity-50 z-0"
+          onClick={handleOverlayClick}
+        />
+      )}
+    </div>
+  );
+};
+
+export default Overlay;
