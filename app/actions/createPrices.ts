d042ef34843ef06bc79c117dda17cf916db73986
molszewski34 <m.olszewski341@gmail.com>
2024-10-22 08:46:21 +0200

    feat: added dates to insert

--- app/actions/createPrices.ts
+++ app/actions/createPrices.ts
@@ -3,13 +3,13 @@
 import { createClient } from '@/utils/supabase/server';
 
 //Function returns rooms id from rooms selected in SetRoom component
-function extractRoomIds(rooms) {
-  return rooms.map(room => ({ id: room.id })); 
+function extractRoomIds(rooms: any) {
+  return rooms.map((room: any) => ({ id: room.id }));
 }
 
 export async function createPrice(priceFormData: any) {
   const supabase = createClient();
-  
+
   const {
     data: { user },
     error: authError,
@@ -26,32 +26,29 @@ export async function createPrice(priceFormData: any) {
   }
 
   const {
-    selectedStartDate,
-    selectedEndDate,
     currentDateTimestamp,
     room,
     selectedRooms,
     partialPrices,
     partialPricesForChildrens,
+    dates,
   } = priceFormData;
 
   const timestampInSeconds = currentDateTimestamp / 1000;
 
-  
   const roomIds = extractRoomIds(selectedRooms);
 
   const { error } = await supabase.from('prices').insert([
     {
-      selected_start_date: selectedStartDate,
-      selected_end_date: selectedEndDate,
       current_date_timestamp: new Date(timestampInSeconds * 1000).toISOString(),
       room: room,
-      selected_rooms: roomIds, 
+      selected_rooms: roomIds,
       partial_prices: partialPrices,
       partial_prices_for_childrens: partialPricesForChildrens,
+      dates: dates,
     },
   ]);
-  
+
   if (error) {
     console.error('Error adding price data:', error);
     return { success: false, error };
