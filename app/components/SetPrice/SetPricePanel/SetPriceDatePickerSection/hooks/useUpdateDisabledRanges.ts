9ea026412870db956b190ad11ff36cdb9429178e
molszewski34 <m.olszewski341@gmail.com>
2024-10-25 11:28:49 +0200

    feat: added query prices, hasOverlap func and const ranges
    On hook start filter prices object and compares to selectedRooms with selected_Rooms in db if found disabled ranges in picker are updated

--- app/components/SetPrice/SetPricePanel/SetPriceDatePickerSection/hooks/useUpdateDisabledRanges.ts
+++ app/components/SetPrice/SetPricePanel/SetPriceDatePickerSection/hooks/useUpdateDisabledRanges.ts
@@ -1,9 +1,47 @@
-import React, { useState } from 'react';
+import { fetchPrices } from '@/app/actions/fetchPrices';
+import { useSetPriceContext } from '@/app/contexts/SetPrice/SetPriceProvider';
+import useSupabaseBrowser from '@/utils/supabase-browser';
+import { useQuery } from '@tanstack/react-query';
+import React, { useEffect, useState } from 'react';
 
 function useUpdateDisabledRanges() {
+  const supabase = useSupabaseBrowser();
+  const { data: pricesResponse, error } = useQuery({
+    queryKey: ['fetchPrices'],
+    queryFn: () => fetchPrices(supabase),
+  });
+
+  const prices = pricesResponse?.data;
+  const { priceFormData } = useSetPriceContext();
+
   const [disabledRanges, setDisabledRanges] = useState<
     { start: Date; end: Date }[]
   >([]);
+
+  useEffect(() => {
+    const hasOverlap = prices.some((price: any) =>
+      price.selected_rooms.some((room: { id: string }) =>
+        priceFormData.selectedRooms.some(
+          (selectedRoom: any) => selectedRoom.id === room.id
+        )
+      )
+    );
+
+    if (hasOverlap) {
+      const ranges = prices.flatMap((price: any) =>
+        price.dates
+          .filter(
+            (date: { startDate: string; endDate: string }) =>
+              date.startDate && date.endDate
+          )
+          .map((date: { startDate: string; endDate: string }) => ({
+            start: new Date(date.startDate),
+            end: new Date(date.endDate),
+          }))
+      );
+      setDisabledRanges(ranges);
+    }
+  }, [priceFormData.selectedRooms, prices]);
 
   const updateDisabledRanges = (pickers: DatePickerItem[]) => {
     const ranges = pickers
@@ -14,7 +52,8 @@ function useUpdateDisabledRanges() {
       }));
     setDisabledRanges(ranges);
   };
-  return { updateDisabledRanges, disabledRanges };
+
+  return { updateDisabledRanges, disabledRanges, setDisabledRanges };
 }
 
 export default useUpdateDisabledRanges;
