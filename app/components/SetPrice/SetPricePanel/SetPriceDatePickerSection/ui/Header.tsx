acc9b7f02a57a73f6436f05626c2f48ae7fa564b
molszewski34 <m.olszewski341@gmail.com>
2024-10-28 08:24:36 +0100

    refactor: imported logic from SetPricePanel

---
+++ app/components/SetPrice/SetPricePanel/SetPriceDatePickerSection/ui/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className="border-b-2 border-gray-200 pb-2 font-semibold text-gray-400 text-[13px]">
+      <h1 className="text-gray-500">Wybierz Termin</h1>
+    </header>
+  );
+};
+
+export default Header;
