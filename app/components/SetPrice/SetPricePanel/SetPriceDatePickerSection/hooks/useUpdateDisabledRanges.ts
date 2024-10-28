0db290ae2695b0758afdbc3c325680779dbea208
molszewski34 <m.olszewski341@gmail.com>
2024-10-28 10:15:09 +0100

    fix, types: removed hasOverlap logic and added con empty object
    Error: when selectedRooms were changed disabledRanger did not change leaving room with wrong disabled ranges displayed on date picker

--- app/components/SetPrice/SetPricePanel/SetPriceDatePickerSection/hooks/useUpdateDisabledRanges.ts
+++ app/components/SetPrice/SetPricePanel/SetPriceDatePickerSection/hooks/useUpdateDisabledRanges.ts
@@ -4,51 +4,62 @@ import useSupabaseBrowser from '@/utils/supabase-browser';
 import { useQuery } from '@tanstack/react-query';
 import React, { useEffect, useState } from 'react';
 
+interface DatePickerItem {
+  startDate: Date;
+  endDate: Date;
+}
+
+interface PriceDate {
+  startDate: string;
+  endDate: string;
+}
+
+interface Price {
+  selected_rooms: { id: string }[];
+  dates: PriceDate[];
+}
+
 function useUpdateDisabledRanges() {
   const supabase = useSupabaseBrowser();
-  const { data: pricesResponse, error } = useQuery({
-    queryKey: ['fetchPrices'],
+  const { priceFormData } = useSetPriceContext();
+
+  const { data: pricesResponse } = useQuery({
+    queryKey: ['fetchPrices', priceFormData.selectedRooms],
     queryFn: () => fetchPrices(supabase),
+    enabled: Boolean(priceFormData.selectedRooms?.length),
   });
 
   const prices = pricesResponse?.data;
-  const { priceFormData } = useSetPriceContext();
-
   const [disabledRanges, setDisabledRanges] = useState<
     { start: Date; end: Date }[]
   >([]);
 
   useEffect(() => {
-    const hasOverlap = prices?.some((price: any) =>
-      price.selected_rooms.some((room: { id: string }) =>
-        priceFormData.selectedRooms.some(
-          (selectedRoom: any) => selectedRoom.id === room.id
-        )
-      )
-    );
-
-    if (hasOverlap) {
-      const ranges = prices.flatMap((price: any) =>
-        price.dates
-          .filter(
-            (date: { startDate: string; endDate: string }) =>
-              date.startDate && date.endDate
+    const ranges =
+      prices?.flatMap((price: Price) =>
+        price.selected_rooms.some((room) =>
+          priceFormData.selectedRooms.some(
+            (selectedRoom: any) => selectedRoom.id === room.id
           )
-          .map((date: { startDate: string; endDate: string }) => ({
-            start: new Date(date.startDate),
-            end: new Date(date.endDate),
-          }))
-      );
-      setDisabledRanges(ranges);
-    }
+        )
+          ? price.dates
+              .filter((date) => date.startDate && date.endDate)
+              .map((date) => ({
+                start: new Date(date.startDate),
+                end: new Date(date.endDate),
+              }))
+          : []
+      ) || [];
+
+    setDisabledRanges(ranges);
   }, [priceFormData.selectedRooms, prices]);
 
   const updateDisabledRanges = (pickers: DatePickerItem[]) => {
     const ranges = pickers
-      .filter((picker: DatePickerItem) => picker.startDate && picker.endDate)
-      .map((picker: DatePickerItem) => ({
-        start: picker.startDate as Date,
-        end: picker.endDate as Date,
+      .filter((picker) => picker.startDate && picker.endDate)
+      .map((picker) => ({
+        start: picker.startDate,
+        end: picker.endDate,
       }));
     setDisabledRanges(ranges);
   };
