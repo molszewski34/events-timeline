4718d7e30409ecf956bee12b04d14babdb1819ed
molszewski34 <m.olszewski341@gmail.com>
2024-11-06 09:56:05 +0100

    refactor: handleRoomSelect now pass selectedRoom as object to selectedRoom state and priceFormData store only ids
    changed it becaus those to states needed separation to handle to separate things. Now selectedRooms is a local state and selectedRoomsIds is state passed as data to db

--- app/components/SetPrice/SetRoom/NextRoom/NextRoom.tsx
+++ app/components/SetPrice/SetRoom/NextRoom/NextRoom.tsx
@@ -4,28 +4,46 @@ import { fetchRooms } from '@/app/actions/fetchRoom';
 import useSupabaseBrowser from '@/utils/supabase-browser';
 import { useSetPriceContext } from '@/app/contexts/SetPrice/SetPriceProvider';
 
+interface Room {
+  id: string;
+  name: string;
+  selectedRoomsIds: string[];
+}
+
 const NextRoom = ({ id }: { id: string }) => {
   const supabase = useSupabaseBrowser();
   const { data: rooms } = useQuery(fetchRooms(supabase, id));
-  const { priceFormData, setPriceFormData } = useSetPriceContext();
+  const { setPriceFormData, selectedRooms, setSelectedRooms } =
+    useSetPriceContext();
 
   const [searchTerm, setSearchTerm] = React.useState('');
 
   const filteredRooms =
     rooms?.filter((room) =>
-      room.name.toLowerCase().includes(searchTerm.toLowerCase())
+      room.name?.toLowerCase().includes(searchTerm.toLowerCase())
     ) || [];
 
-  const handleRoomSelect = (selectedRoom: any) => {
-    setPriceFormData((prev) => ({
+  const handleRoomSelect = (selectedRoom: Room) => {
+    const isRoomSelected = selectedRooms.some(
+      (roomId: string) => roomId === selectedRoom.id
+    );
+
+    setSelectedRooms(
+      isRoomSelected
+        ? selectedRooms.filter((roomId: string) => roomId !== selectedRoom.id)
+        : [...selectedRooms, selectedRoom.id]
+    );
+
+    setPriceFormData((prev: Room) => ({
       ...prev,
-      selectedRooms: prev.selectedRooms.some(
-        (room) => room.id === selectedRoom.id
-      )
-        ? prev.selectedRooms.filter((room) => room.id !== selectedRoom.id) //
-        : [...prev.selectedRooms, selectedRoom],
+      selectedRoomsIds: isRoomSelected
+        ? prev.selectedRoomsIds.filter(
+            (roomId: string) => roomId !== selectedRoom.id
+          )
+        : [...prev.selectedRoomsIds, selectedRoom.id],
     }));
   };
+
   return (
     <div className="flex flex-col p-2 absolute top-6 bg-white border-2 border-gray-100 w-full rounded-sm">
       <header className="pt-2 font-semibold text-gray-400 text-[13px]">
@@ -34,17 +52,25 @@ const NextRoom = ({ id }: { id: string }) => {
 
       <div className="flex mt-2 border rounded border-gray-300 gap-2 py-1 px-2">
         <div className="flex gap-2">
-          {priceFormData.selectedRooms?.map((room) => (
-            <button
-              key={room.id}
-              className="flex flex-nowrap gap-2 items-center justify-between text-xs text-white bg-[#00a541] py-1 px-2 rounded h-[23px]"
-              onClick={() => handleRoomSelect(room)}
-            >
-              <p className="flex flex-wrap whitespace-nowrap">{room.name}</p>
-              <i className="text-sm">cancel</i>
-            </button>
-          ))}
+          {selectedRooms?.map((roomId: string) => {
+            const room = rooms?.find((room) => room.id === roomId);
+            return (
+              room && (
+                <button
+                  key={room.id}
+                  className="flex flex-nowrap gap-2 items-center justify-between text-xs text-white bg-[#00a541] py-1 px-2 rounded h-[23px]"
+                  onClick={() => handleRoomSelect(room)}
+                >
+                  <p className="flex flex-wrap whitespace-nowrap">
+                    {room.name}
+                  </p>
+                  <i className="text-sm">cancel</i>
+                </button>
+              )
+            );
+          })}
         </div>
+
         <input
           type="text"
           className="w-full h-[23px] p-1"
